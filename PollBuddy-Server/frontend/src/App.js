da0e15213a46c6f805bbae75376ad36b8d90ea99
hbh7 <hbh7@hbh7.com>
2019-10-08 21:22:14 -0400

    Added in old helloworld message

--- PollBuddy-Server/frontend/src/App.js
+++ PollBuddy-Server/frontend/src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Welcome to PollBuddy!!!!
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
