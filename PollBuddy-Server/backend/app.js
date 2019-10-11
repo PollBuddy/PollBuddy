e86d75d527aadc948c0b7f814fd2197395a02594
hbh7 <hbh7@hbh7.com>
2019-10-11 18:18:20 -0400

    Added basic routing functionality based on gdoc specification

--- PollBuddy-Server/backend/app.js
+++ PollBuddy-Server/backend/app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var classesRouter = require('./routes/classes');
+var pollsRouter = require('./routes/polls');
 var usersRouter = require('./routes/users');
 
 // In case we run into CORS issues
@@ -25,8 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/classes', classesRouter);
+app.use('/api/polls', pollsRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- PollBuddy-Server/backend/routes/users.js
+++ PollBuddy-Server/backend/routes/users.js
@@ -1,9 +1,22 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  next(createError(405));
+});
+
+router.get('/login/', function(req, res, next) {
+  res.send('i am a login processor');
+});
+
+router.get('/userID/', function(req, res, next) {
+  res.send('i am a user ID processor');
+});
+
+router.get('/login/', function(req, res, next) {
+  res.send('i am a user ID classes processor');
 });
 
 module.exports = router;
