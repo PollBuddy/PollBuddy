294eeb7fe49d676b65c83a3b56cddc301f174db3
Gustav Serger <gustav.serger@gmail.com>
2019-10-22 16:51:28 -0400

    make lesson page with reactjs-component-generator

---
+++ PollBuddy-Server/frontend/src/pages/lesson/lesson.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './lesson.scss'
+export default class lesson extends Component {
+  render() {    
+    return (
+        <div className="page-lesson">
+        Hello lesson 
+      </div>
+    )
+  }
+}
