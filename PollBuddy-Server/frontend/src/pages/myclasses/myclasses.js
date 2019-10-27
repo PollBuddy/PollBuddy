cb4c0fe57e334c0099b36d3965534f5b40fa2e6a
Gustav Serger <gustav.serger@gmail.com>
2019-10-27 14:52:15 -0400

    chore: rearrange app.js. Move contents to homepage page, make myclasses page, and set up routes between them.

---
+++ PollBuddy-Server/frontend/src/pages/myclasses/myclasses.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './myclasses.scss'
+export default class myclasses extends Component {
+  render() {    
+    return (
+      <div className="page-my-classes">
+        Hello myClasses 
+      </div>
+    )
+  }
+}
