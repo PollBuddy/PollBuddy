4fa3fe7b1909fcc445241a0fe6f462e6413ac57d
nbezanis <nbezanis98@gmail.com>
2019-10-27 15:16:42 -0400

    create rough ui for question component that reads from dummy json

--- PollBuddy-Server/frontend/src/App.js
+++ PollBuddy-Server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Question from "./pages/lesson/components/question";
 
 function App() {
 
@@ -13,39 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Welcome to PollBuddy!!!!
-        </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        </header>
-
-      <body>
-      <p>
-        hey yall.
-      </p>
-
-      <button onClick={toBlue}>
-        Click me!
-      </button>
-
-      <button onClick={toRed}>
-        No, click me!
-      </button>
-
-      </body>
+      <Question/>
     </div>
   );
 }
--- PollBuddy-Server/frontend/src/pages/lesson/components/question/question.js
+++ PollBuddy-Server/frontend/src/pages/lesson/components/question/question.js
@@ -1,15 +1,50 @@
 import React, {Component} from 'react';
 import './question.scss'
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCol,
+    MDBContainer,
+} from 'mdbreact';
 // import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
 // import * as questionActions from "../../store/question/actions";
+
+//this is a placeholder funcion, it will eventually be used
+//to get information about the question from the database
+const getDataFromJSON = () =>{
+    let data = require('./dummyInfo');
+    return data;
+};
+
 export default class question extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {};
-    // }
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-      return <div className="component-question">Hello! component question</div>;
+        let data = getDataFromJSON();
+        return (
+            <MDBCol>
+                <MDBCard style={{ width: "22rem" }}>
+                    <MDBCardImage className="img-fluid" src={data.img} waves />
+                    <MDBCardBody>
+                        <MDBCardTitle>{data.title}</MDBCardTitle>
+                        <MDBCardText>{data.question}</MDBCardText>
+                        <MDBCardText>
+                            {data.choices.map((choice, index) => (
+                                <MDBContainer>
+                                    <button className="choiceButton">{data.choices[index]}</button>
+                                </MDBContainer>
+                            ))}
+                        </MDBCardText>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+        );
     }
   }
 // export default connect(
