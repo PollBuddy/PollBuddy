98041854c7e41d1d903ba9469be90955ea59cd27
neha-deshpande001 <neha-deshpande001@users.noreply.github.com>
2019-10-31 16:29:17 -0400

    finished rough myclasses page

--- PollBuddy-Server/frontend/src/pages/myclasses/myclasses.js
+++ PollBuddy-Server/frontend/src/pages/myclasses/myclasses.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import { MDBBtn } from "mdbreact";
+import { MDBBtn, MDBIcon } from "mdbreact";
 import 'mdbreact/dist/css/mdb.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import homepage from '../homepage'
 import logo from '../../logo.svg';
 
 import './myclasses.scss'
@@ -12,25 +12,42 @@ export default class Myclasses extends Component {
 
     return (
       <div className="page-my-classes">
-          <homepage></homepage>{}
-        <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p1>
-          My Classes
-        </p1>
-        <MDBBtn
+          <img src={logo} className="top_left_logo" alt="logo"/>
+
+          <font size="+3">My Classes</font>
+
+          <MDBBtn
+            size="sm"
+            className="home_button"
             href="/"
             color="secondary"
-        >
-            HOME
-        </MDBBtn>
+          >
+            <MDBIcon icon="home" />
+          </MDBBtn>
+
+
+          <MDBBtn
+              size="sm"
+              className="settings_button"
+              href="/"
+              color="secondary"
+          >
+            <MDBIcon icon="tools" />
+          </MDBBtn>
+
+        <hr></hr>
+
 
 
         <body>
-          body
+        CSCI 1200
         </body>
 
-          </div>
+
+        <div id="header-content">
+          You have joined CSCI 1200
+        </div>
+
       </div>
     )
   }
