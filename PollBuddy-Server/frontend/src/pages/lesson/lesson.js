188ab0329cf1a0cb54791dd135a4b71b513e9b2c
Gustav Serger <gustav.serger@gmail.com>
2019-11-01 15:02:57 -0400

    basic layout to stack questioncomponents

--- PollBuddy-Server/frontend/src/pages/lesson/lesson.js
+++ PollBuddy-Server/frontend/src/pages/lesson/lesson.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import './lesson.scss'
+
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact'
 export default class lesson extends Component {
 
-  render() {    
+  
+  render() {
+    const questions = [1,2,3,4,5];
+    function addq() {
+      questions.concat(1);
+    }
+    
     return (
-      <div className="page-lesson">
-        Hello lesson {this.props.lessonId}
-      </div>
+      <MDBContainer>
+        <div className="page-lesson">
+          Hello lesson {this.props.lessonId}
+        </div>
+        <MDBContainer>
+          {questions.map((value, index) => {
+            return (
+            <MDBRow key={index}>
+              <MDBCol>{value}</MDBCol>
+            </MDBRow>)
+          })}
+        </MDBContainer>
+      </MDBContainer>
     )
   }
 }
