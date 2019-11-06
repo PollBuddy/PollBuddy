c01f8ea19155c69ba4cfd391975fccbdeac37e15
nbezanis <nbezanis98@gmail.com>
2019-11-06 16:33:08 -0500

    make it so that it deselects everything when the user chooses too many options and added comments

--- PollBuddy-Server/frontend/src/pages/lesson/components/question/question.js
+++ PollBuddy-Server/frontend/src/pages/lesson/components/question/question.js
@@ -24,13 +24,18 @@ const getDataFromJSON = () =>{
 export default class question extends Component {
     constructor(props) {
         super(props);
+        //binding helper functions
         this.deselectChoice = this.deselectChoice.bind(this);
         this.selectChoice = this.selectChoice.bind(this);
+        //get the data from the json and store it as an object
         let data = getDataFromJSON();
+        //set up an array of booleans (representing the student's answer choices)
+        //and initialize it to all false
         let tempArray = [];
         for(let i = 0; i < data.choices.length; i++){
             tempArray.push(false);
         }
+        //add the data and the array to state
         this.state = {
             data: data,
             studentChoices: tempArray,
@@ -38,6 +43,8 @@ export default class question extends Component {
     }
 
     deselectChoice(index){
+        //set the boolean in the array at the selected index to false
+        //and update state
         let tempChoices = this.state.studentChoices;
         tempChoices[index] = false;
         this.setState(prevState => ({
@@ -51,19 +58,23 @@ export default class question extends Component {
     selectChoice(index){
         let tempChoices = this.state.studentChoices;
         let count = 0;
+        //count the number of booleans that are true in the array
         for(let i = 0; i < this.state.studentChoices.length; i++){
             if(this.state.studentChoices[i]){
                 count++;
             }
         }
+        //if the number of true booleans is greater than the maximum number of
+        //allowed choices (specified by the json) then set the entire array
+        //back to false
         if(count >= this.state.data.maxAllowedChoices){
             for(let i = 0; i < this.state.studentChoices.length; i++){
                 if(this.state.studentChoices[i]){
                     tempChoices[i] = false;
-                    break;
                 }
             }
         }
+        //make the boolean at the selected index true and update state
         tempChoices[index] = true;
         this.setState(prevState => ({
             ...prevState,
