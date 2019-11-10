c5aa4eff8157ecf4ccc4a218068f82de47def639
AnuC01 <55463008+AnuC01@users.noreply.github.com>
2019-11-10 01:59:30 -0500

    Add files via upload
    Updated Login Page

--- PollBuddy-Server/frontend/Login Page/login.js
+++ PollBuddy-Server/frontend/Login Page/login.js
@@ -1,43 +1,60 @@
 import React, {Component} from 'react';
 import './login.scss'
-import { MDBBtn } from "mdbreact";
 import 'mdbreact/dist/css/mdb.css';
-import { MDBInput } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 
-//purple color: f3e1ff
 export default class login extends Component {
   render() {    
     return (
-        <div className="page-login">
+     <div className="page-login">
 
-            <MDBBtn color = "secondary">About Us</MDBBtn>
+          <MDBBtn size = "lg"  color = "secondary">About Us</MDBBtn>
 
-          <header className="App-header">
+        <header className="Login-header">
             <h2>
               PollBuddy
             </h2>
-          </header>
+        </header>
 
-          <header Login = "LoginElements">
+        <div className="d-flex p-2 Login-Box">
 
-              <p>
-                  Username:
-              </p>
+            <header Login = "LoginElements">
 
-            <MDBInput label="Username" className="mt-4" />
+                <MDBContainer>
+                    <MDBRow>
+                        <MDBCol md="6">
+                            <form>
+                                <p className="h5 text-center mb-4">Sign in</p>
+                                <div className="white-text">
+                                    <MDBInput
+                                        label="Type your email"
+                                        icon="envelope"
+                                        group
+                                        type="email"
+                                        validate
+                                        error="wrong"
+                                        success="right"
+                                    />
+                                    <MDBInput
+                                        label="Type your password"
+                                        icon="lock"
+                                        group
+                                        type="password"
+                                        validate
+                                    />
+                                </div>
+                            <div className="text-right">
+                                <MDBBtn size = "lg" color = "secondary">Sign In</MDBBtn>
+                                <MDBBtn size = "lg" color = "secondary">Forgot Your Password</MDBBtn>
+                            </div>
+                        </form>
+                    </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
 
-            <p>
-              Password:
-            </p>
+             </header>
 
-              <MDBInput label="Password" className="mt-4" />
-
-              <MDBBtn color = "secondary">Login</MDBBtn>
-
-              <MDBBtn color = "secondary">Forgot Your Password</MDBBtn>
-
-
-            </header>
+         </div>
 
         </div>
     )
