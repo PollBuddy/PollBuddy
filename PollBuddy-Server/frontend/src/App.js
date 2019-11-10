6c0a371b3db4f83f2e4a47cec59d963c74ab5987
Gustav Serger <gustav.serger@gmail.com>
2019-11-10 01:41:14 -0500

    temp fix mdb css

--- PollBuddy-Server/frontend/src/App.js
+++ PollBuddy-Server/frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Router, Link } from "@reach/router";
 
+// ---- MDBReact ----
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 import './App.css';
 import Homepage from './pages/homepage'
