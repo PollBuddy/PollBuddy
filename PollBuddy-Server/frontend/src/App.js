75e185b50ca24aa55974b4527a4aa7c371784e1f
Gustav Serger <gustav.serger@gmail.com>
2019-11-10 00:11:21 -0500

    fix: mdbreact imports

--- PollBuddy-Server/frontend/src/App.js
+++ PollBuddy-Server/frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Router } from "@reach/router";
 
+// ---- MDBReact ----
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 import './App.css';
 import Myclasses from './pages/myclasses'
