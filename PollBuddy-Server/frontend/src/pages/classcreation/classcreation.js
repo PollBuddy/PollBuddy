b1406cf8ae07c82f2ea31703c6157b14c7a955de
AnuC01 <55463008+AnuC01@users.noreply.github.com>
2019-12-05 20:13:13 -0500

    Create classcreation.js
    Created Class Creation Page for Professors to Create a Class

---
+++ PollBuddy-Server/frontend/src/pages/classcreation/classcreation.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import './classcreation.scss'
+import 'mdbreact/dist/css/mdb.css';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+
+export default class classcreation extends Component {
+  render() {    
+    return (
+     <div className="page-classcreation">
+
+        <header className="Classcreation-header">
+            <h>
+              Create Your Classroom
+            </h>
+        </header>
+
+        <div className="d-flex p-2 Classcreation-Box">
+
+            <header Classcreation = "ClasscreationElements">
+
+                <MDBContainer>
+                    <MDBRow>
+                        <MDBCol md="6">
+                            <form>
+                                <p className="h5 text-center mb-4">Enter Your Classroom Name Below</p>
+                                <div className="white-text">
+                                    <MDBInput
+                                        label="Enter name"
+                                        icon="envelope"
+                                        group
+                                        type="email"
+                                        validate
+                                        error="wrong"
+                                        success="right"
+                                    />
+
+                                    <p className="h5 text-center mb-4">Enter Your Classroom Description Below </p>
+                                    <MDBInput
+                                        label="Enter classroom description"
+                                        icon="lock"
+                                        group
+                                        type="password"
+                                        validate
+                                    />
+                                </div>
+                                <MDBBtn size = "sm" color = "secondary">Create Classroom</MDBBtn>
+                        </form>
+                    </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+
+             </header>
+
+         </div>
+
+        </div>
+    )
+  }
+}
