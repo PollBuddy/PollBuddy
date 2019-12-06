29ba859d69dfc9a1fccc623852ebc67228330f30
AnuC01 <55463008+AnuC01@users.noreply.github.com>
2019-12-05 20:11:08 -0500

    Add files via upload
    Updated App.js to Account for Class Creation Page

--- PollBuddy-Server/frontend/src/App.js
+++ PollBuddy-Server/frontend/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 
-// ---- MDBReact ----
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 
 import './App.css';
-import Homepage from './pages/homepage'
 import Myclasses from './pages/myclasses'
-import Lessons from './pages/lessons';
-import Lesson from './pages/lesson';
-import Notfound from './pages/notfound'
+import Homepage from './pages/homepage'
+import Login from './pages/login'
+import Classcreation from './pages/classcreation'
 
 function App() {
 
@@ -23,11 +18,10 @@ function App() {
         Link to them with link tags
       */}  
       <Router>
-        <Homepage path="/" />
         <Myclasses path="/myclasses" />
-        <Lessons path="/lessons" />
-        <Lesson path="/lesson/:lessonId" />
-        <Notfound default />
+        <Homepage path="/" />
+        <Login path= "/login" />
+        <Classcreation path= "/classcreation" />
       </Router>
     </React.Fragment>
   );
