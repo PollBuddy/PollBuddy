1320e60dc9ab51dccc4d8ed6ed9d4c467021a11f
Neha Deshpande <neha.deshpande001@gmail.com>
2020-02-03 12:25:52 -0500

    changed color scheme and format
    Colors - purple, black, white.
    Format - added arrows to scroll down on the page so login, sign up, and about are all on one page. Need to work on login, sign up, and about. Also need to change the logo.

--- PollBuddy-Server/frontend/src/pages/homepage/homepage.js
+++ PollBuddy-Server/frontend/src/pages/homepage/homepage.js
@@ -1,30 +1,72 @@
 import React, {Component} from 'react';
 import './homepage.scss'
 import 'mdbreact/dist/css/mdb.css';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBIcon } from 'mdbreact';
 import { Router, Link } from '@reach/router';
+import new_logo from '../../Poll_buddy_logo_v5.png';
 
+//MoolBoran
 export default class homepage extends Component {
   render() {
     return (
         <MDBContainer className="page-homepage">
-            <header className="Homepage-header">
+            <header className="Homepage-top">
+                <img src={new_logo} className="top_left_logo" alt="logo"/>
 
                 <h1>
                     Welcome to PollBuddy!
                 </h1>
 
                 <MDBContainer className="d-flex p-2 Homepage-box">
-                    PollBuddy is an interactive platform for educators to connect with their students in a virtual classroom, where
-                    educators can ask questions during class.
+                    PollBuddy is an interactive platform for educators to connect with students in a virtual classroom.
                 </MDBContainer>
 
                 <MDBContainer className="text-right">
-                    <Link to={"/login"}>
-                        <MDBBtn size = "lg"  color = "secondary">Sign In</MDBBtn>
-                    </Link>
-                    <MDBBtn size = "lg"  color = "secondary">Sign Up</MDBBtn>
+                    <MDBBtn size = "lg" href="#signin" outline color="purple darken-1">Sign In</MDBBtn>
+
+                    <MDBBtn size = "lg" href="#signup" outline color="purple darken-1">Sign Up</MDBBtn>
+
+
                 </MDBContainer>
+                <MDBContainer className="top_left_logo">
+
+                <MDBBtn size = "lg" href="#about" outline color="purple darken-1">About</MDBBtn>
+        </MDBContainer>
+
+                <MDBContainer className="top_left_logo">
+                </MDBContainer>
+
+
+
+                <MDBContainer className="arrow" >
+                    <MDBBtn href="#signin" floating size="lg" color="gray"><MDBIcon icon="arrow-down" /></MDBBtn>
+                </MDBContainer>
+
+                <header className="Homepage-header" id="signin">
+                    <h2>
+                        Login Here
+                    </h2>
+                </header>
+
+                <MDBContainer className="arrow" >
+                    <MDBBtn href="#signup" floating size="lg" color="gray"><MDBIcon icon="arrow-down" /></MDBBtn>
+                </MDBContainer>
+
+                <header className="Homepage-header" id="signup">
+                    <h2>
+                        Sign up Here
+                    </h2>
+                </header>
+
+                <MDBContainer className="arrow" >
+                    <MDBBtn href="#about" floating size="lg" color="gray"><MDBIcon icon="arrow-down" /></MDBBtn>
+                </MDBContainer>
+
+                <header className="Homepage-header" id="about">
+                    <h2>
+                        About PollBuddy
+                    </h2>
+                </header>
 
             </header>
         </MDBContainer>
