5dc85caf2cdbb4f1daccdc3343c729247fd0edc5
Neha Deshpande <neha.deshpande001@gmail.com>
2020-02-04 17:44:27 -0500

    Removed arrows/scrolling because Hunter didn't like it
    Also added the "Enter Poll Code" button

--- PollBuddy-Server/frontend/src/pages/homepage/homepage.js
+++ PollBuddy-Server/frontend/src/pages/homepage/homepage.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './homepage.scss'
 import 'mdbreact/dist/css/mdb.css';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBIcon } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 import { Router, Link } from '@reach/router';
 import new_logo from '../../Poll_buddy_logo_v5.png';
 
@@ -21,54 +21,15 @@ export default class homepage extends Component {
                     PollBuddy is an interactive platform for educators to connect with students in a virtual classroom.
                 </MDBContainer>
 
-                <MDBContainer className="text-right">
-                    <MDBBtn size = "lg" href="#signin" outline color="purple darken-1">Sign In</MDBBtn>
-
-                    <MDBBtn size = "lg" href="#signup" outline color="purple darken-1">Sign Up</MDBBtn>
-
-
+                <MDBContainer className="top_right_button">
+                    <MDBBtn size = "lg" href="/login" outline color="purple darken-1">Login/Register</MDBBtn>
                 </MDBContainer>
-                <MDBContainer className="top_left_logo">
-
-                <MDBBtn size = "lg" href="#about" outline color="purple darken-1">About</MDBBtn>
-        </MDBContainer>
-
-                <MDBContainer className="top_left_logo">
+                <MDBContainer className="poll-code">
+                    <MDBInput label="Enter Poll Code" size="sm" class="poll-code"/>
                 </MDBContainer>
 
 
-
-                <MDBContainer className="arrow" >
-                    <MDBBtn href="#signin" floating size="lg" color="gray"><MDBIcon icon="arrow-down" /></MDBBtn>
-                </MDBContainer>
-
-                <header className="Homepage-header" id="signin">
-                    <h2>
-                        Login Here
-                    </h2>
-                </header>
-
-                <MDBContainer className="arrow" >
-                    <MDBBtn href="#signup" floating size="lg" color="gray"><MDBIcon icon="arrow-down" /></MDBBtn>
-                </MDBContainer>
-
-                <header className="Homepage-header" id="signup">
-                    <h2>
-                        Sign up Here
-                    </h2>
-                </header>
-
-                <MDBContainer className="arrow" >
-                    <MDBBtn href="#about" floating size="lg" color="gray"><MDBIcon icon="arrow-down" /></MDBBtn>
-                </MDBContainer>
-
-                <header className="Homepage-header" id="about">
-                    <h2>
-                        About PollBuddy
-                    </h2>
-                </header>
-
-            </header>
+  </header>
         </MDBContainer>
     )
   }
