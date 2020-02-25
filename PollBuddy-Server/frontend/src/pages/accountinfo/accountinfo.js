e9ee41a3beb93b1d1ca26c1b85c77dafcc3e6736
Ashwin Chitoor <ashketchup55@gmail.com>
2020-02-25 16:48:20 -0500

    added basic construct

---
+++ PollBuddy-Server/frontend/src/pages/accountinfo/accountinfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./accountinfo.scss"
+import 'mdbreact/dist/css/mdb.css';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+
+export default class accountinfo extends Component {
+    render() {
+      return (
+
+
+
+      )
+    }
+}
