93d354e98b22b32afe9822b7b37550e1308448ec
nbezanis <nbezanis98@gmail.com>
2020-02-25 16:36:44 -0500

    add state and constructor to class creator component

--- PollBuddy-Server/frontend/src/components/classEditor/classEditor.js
+++ PollBuddy-Server/frontend/src/components/classEditor/classEditor.js
@@ -3,6 +3,20 @@ import './classEditor.scss'
 import {MDBContainer} from "mdbreact";
 
 export default class ClassEditor extends Component {
+    constructor(){
+        super();
+        //holds a variable for each piece of information that the backend
+        //has
+        this.state = {
+            name: '',
+            id: '',
+            polls: [],
+            users: [],
+            instructors: []
+        }
+    }
+
+
     render() {
         return <MDBContainer>
             <h>test</h>
