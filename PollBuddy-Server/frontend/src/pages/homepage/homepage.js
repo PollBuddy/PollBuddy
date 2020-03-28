3c1d778e87b2840bfdc4e433488c64b9e73d028a
hbh7 <hbh7@hbh7.com>
2020-03-27 23:39:04 -0400

    Fixed the terrible merging job I apparently did before

--- PollBuddy-Server/frontend/src/pages/homepage/homepage.js
+++ PollBuddy-Server/frontend/src/pages/homepage/homepage.js
@@ -3,34 +3,29 @@ import './homepage.scss'
 import 'mdbreact/dist/css/mdb.css';
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 import { Router, Link } from '@reach/router';
-import new_logo from '../../Poll_buddy_logo_v5.png';
 
-//MoolBoran
 export default class homepage extends Component {
-  render() {
-    return (
-        <MDBContainer className="page-homepage">
-            <header className="Homepage-top">
-                <img src={new_logo} className="top_left_logo" alt="logo"/>
+    render() {
+        return (
+            <MDBContainer className="page-homepage">
+                <header className="Homepage-header">
 
-                <img src="logo.svg" class="img-fluid animated bounce infinite logo">
-                </img>
+                    <img src="logo.svg" class="img-fluid animated bounce infinite logo">
+                    </img>
 
-                <p className = "blurb"> An interactive questionnaire platform made by students, for 
-                students, to strengthen lecture material and class attentiveness.
-                </p>
+                    <p className = "blurb"> An interactive questionnaire platform made by students, for
+                        students, to strengthen lecture material and class attentiveness.
+                    </p>
 
-                <MDBContainer className="text-right">
-                    <Link to={"/login"}>
-                        <button class = "btn button">Sign In</button>
-                    </Link>
-                    <button class = "btn button">Sign Up</button>
-      
-                </MDBContainer>
+                    <MDBContainer className="text-right">
+                        <Link to={"/login"}>
+                            <button class = "btn button">Sign In</button>
+                        </Link>
+                        <button class = "btn button">Sign Up</button>
+                    </MDBContainer>
 
-
-  </header>
-        </MDBContainer>
-    )
-  }
+                </header>
+            </MDBContainer>
+        )
+    }
 }
