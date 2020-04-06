037d04241dee7a90bda1c0e44970ec8fd80bf538
Annie <annieshi6@gmail.com>
2020-04-06 06:07:45 -0400

    all register pages have now been updated

--- PollBuddy-Server/frontend/src/pages/registerWithPollBuddy/registerWithPollBuddy.js
+++ PollBuddy-Server/frontend/src/pages/registerWithPollBuddy/registerWithPollBuddy.js
@@ -4,17 +4,13 @@ import { MDBContainer } from 'mdbreact';
 import 'mdbreact/dist/css/mdb.css';
 import new_logo from "../../Poll_Buddy_Logo_v6.png";
 
+import Header from "../../components/header/header.js"
 
 export default class registerWithPollBuddy extends Component {
   render() {
     return (
     		<MDBContainer fluid className="register-with-school">
-    			<MDBContainer fluid className="header">
-                    <img src={new_logo} alt="logo" className="poll-logo"/>
-	    			<form>
-                        <button className = "btn sign-button" formAction="/login" >Sign in</button>
-                    </form>
-    			</MDBContainer>
+    	        <Header title = "Register with us" type = "in" />
                 <MDBContainer fluid className="p-b-box">
                     <h1 className="register-text">
                         Register with Poll Buddy
