2e0d596127cef6bb97f1d1d16b7f3e06934f926b
Neha Deshpande <neha.deshpande001@gmail.com>
2020-04-22 02:02:18 -0400

    Refactored the privacy page & added resouces
    Previous styling didn't work properly on my computer, so I changed the css file. I also added a couple links that will help us write a privacy policy

--- PollBuddy-Server/frontend/src/pages/privacy/privacy.js
+++ PollBuddy-Server/frontend/src/pages/privacy/privacy.js
@@ -2,33 +2,45 @@ import React, { Component } from 'react';
 import "./privacy.scss"
 import 'mdbreact/dist/css/mdb.css';
 import { MDBContainer } from 'mdbreact';
-import logo from '../../images/logo.png';
+import Header from "../../components/header/header.js"
 
-export default class accountinfo extends Component {
+export default class privacy extends Component {
     componentDidMount(){
         document.title = "Privacy - " + document.title;
     }
     render() {
       return (
         <MDBContainer>
-          <header>
-          <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet"></link>
-            <div className="top-bar">
-              <div className="header-text">
-                <img src={logo} alt="logo" className="logo img-fluid"/>
-                  Privacy
-              </div>              
-            </div>
-            <div className="main-body text-center">
-            <br></br>
-                <div style={{fontSize: 50 + 'px'}}>From the contributers of Poll Buddy,</div> <br></br> <br></br>
-                  Our promise is to keep your data safe, protected and far from anyone 
-                  that could use it in a harmful way. <br></br>The purpose of this app is and always will be
-                  for educational purposes <i>only</i>. 
+            <Header title = "privacy" btn = "login" />
 
-            </div>
-             
-          </header>
+
+            <br></br>
+
+            <p className="placeholder-text">
+            From the contributors of Poll Buddy,<br/>
+
+            Our promise is to keep your data safe, protected and far from anyone that could use it in a harmful way.
+            The purpose of this app is and always will be
+            for educational purposes <i>only</i>.
+            <br/>
+            etc. etc.
+            <br/>
+            (We will definitely need to rewrite the above text)
+            <br/><br/>
+            Here's some information about privacy policies. Pretty much, we need to talk about what info we will take,
+            how we will use it, how we will store/protect it, cookies, etc. At one point in the near future, we should gather together and write a privacy policy.
+            <br/><br/>
+            https://www.privacypolicytemplate.net/
+            <br/>
+            https://www.opentracker.net/article/how-write-website-privacy-policy
+            <br/><br/>
+            After developing our privacy policy, it would be cool to write it all in a google doc/markdown/whatever file
+            and then just display it on this page. I'm not sure how plausible that would be, but editing would be much easier.
+            Regular text on this .js file is very difficult to update and maintain.
+            <br/><br/>
+            </p>
+
+
 
         </MDBContainer>
       )
