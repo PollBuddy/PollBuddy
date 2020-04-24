a93ed001cfd6e99c29ee36e58d9def3127b17fc9
Ashwin Chitoor <ashketchup55@gmail.com>
2020-04-24 14:08:33 -0400

    changed to the new header and cleaned up page

--- PollBuddy-Server/frontend/src/pages/privacy/privacy.js
+++ PollBuddy-Server/frontend/src/pages/privacy/privacy.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./privacy.scss"
 import 'mdbreact/dist/css/mdb.css';
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+import Header from "../../components/header/header.js"
 
 export default class accountinfo extends Component {
     render() {
@@ -9,18 +10,24 @@ export default class accountinfo extends Component {
         <MDBContainer>
           <header>
           <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet"></link>
-            <div className="top-bar">
-              <div className="header-text">
-                <img src="Poll-Buddy-Logo.png" alt="logo" className="logo img-fluid"></img>
-                  Privacy
-              </div>              
-            </div>
+
+          <Header title = "Privacy" btn = "account" />
             <div className="main-body text-center">
             <br></br>
-                <div style={{fontSize: 50 + 'px'}}>From the contributers of Poll Buddy,</div> <br></br> <br></br>
-                  Our promise is to keep your data safe, protected and far from anyone 
-                  that could use it in a harmful way. <br></br>The purpose of this app is and always will be
-                  for educational purposes <i>only</i>. 
+            <MDBRow>  
+              <MDBCol sm="10">
+                  <div className="top">
+                      From the contributers of Poll Buddy,
+                  </div>
+                  <br></br>
+                  <div className="body-words">
+                    Our promise is to keep your data safe, protected and far from anyone 
+                    that could use it in a harmful way. <br></br>The purpose of this app is and always will be
+                    for educational purposes <i>only</i>. 
+                  </div>
+              </MDBCol>
+
+            </MDBRow>                  
 
             </div>
              
