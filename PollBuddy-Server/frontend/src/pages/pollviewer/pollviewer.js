710a1e25f21a3d437db8d4500926e7ce12ab81aa
Ashwin Chitoor <ashketchup55@gmail.com>
2020-04-24 12:42:57 -0400

    added poll quesitons

--- PollBuddy-Server/frontend/src/pages/pollviewer/pollviewer.js
+++ PollBuddy-Server/frontend/src/pages/pollviewer/pollviewer.js
@@ -17,6 +17,16 @@ export default class accountinfo extends Component {
                         <div className="question-number">
                             Question 3 of 28:
                         </div>
+                        <div className="question">
+                            Why does the tooth fairy collect teeth?
+                        </div>
+
+                        <ul className="answers">
+                            <li>She grinds them into the fairy dust she needs to fly</li>
+                            <li>She  gives them to new babies who are ready to grow teeth</li>
+                            <li>She gives the good teeth to dentists to make false teeth</li>
+                            <li>She grinds them up and makes sand for the beach</li>
+                        </ul>
                     </MDBCol>
                 </MDBRow>
                 
