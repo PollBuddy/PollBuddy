e9c4c23847e0af005af170befda3dec6b001856b
Annie <annieshi6@gmail.com>
2020-04-30 03:53:42 -0400

    new style for footer w addition of link to rpi accessibility statement

--- PollBuddy-Server/frontend/src/components/footer/footer.js
+++ PollBuddy-Server/frontend/src/components/footer/footer.js
@@ -7,20 +7,26 @@ import github_logo from '../../github.png'
 export default class Footer extends Component {
 	render() {
 		return (
-			<div className = "foot">
+			<footer className = "foot">
+				<div className = "linethru"></div>
 				<div className = "logo_links">
-					<a href = "https://rcos.io/" target = "_blank">
+					<a href = "https://rcos.io/" target = "_blank" rel="noopener noreferrer">
 						<img src = {rcos_logo} alt = "RCOS" />
 					</a>
-					<a href = "https://github.com/PollBuddy/PollBuddy" target = "_blank">
+					<a href = "https://github.com/PollBuddy/PollBuddy" target = "_blank" rel="noopener noreferrer">
 						<img src = {github_logo} alt = "Github" />
 					</a>
 				</div>
-				<a href = "/">
-					About Us
-				</a>
-				Questions, comments, etc? Email us: someemail@yeah.com
-			</div>
+				<div className = "foot_text">
+					<a href = "/">
+						About Us
+					</a>
+					<a href = "https://info.rpi.edu/statement-of-accessibility" target = "_blank" rel = "noopener noreferrer">
+						Statement of Accessibility
+					</a>
+					Questions, comments, etc? Email us: someemail@yeah.com
+				</div>
+			</footer>
 		)
 	}
 }
