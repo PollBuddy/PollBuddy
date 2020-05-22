56761de69c82b6550427f73ecf1f8c07f75e041a
hbh7 <hbh7@hbh7.com>
2020-05-22 19:59:56 -0400

    Added an example .env file and made some tweaks to the mongo module

--- PollBuddy-Server/backend/modules/mongoConnection.js
+++ PollBuddy-Server/backend/modules/mongoConnection.js
@@ -9,16 +9,16 @@ module.exports = {
 		con();
 
 		function con() {
-			const client = new MongoClient(process.env['MONGO_URL']);
+			const client = new MongoClient(process.env.DB_URL);
 			client.connect((err) => {
 				if (err) {
 					console.error("Seems the database isn't up yet, retrying in 1 second");
 					setTimeout(function () {
 						con();
-					}, 1000);
+					}, 3000);
 				} else {
-					db = client.db(process.env['MONGO_DB']);
-					console.log('Database connected from module');
+					db = client.db(process.env.DB_NAME);
+					console.log('Database connected');
 				}
 			});
 
