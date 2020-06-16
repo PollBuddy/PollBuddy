fe50795de2ccb58ed2bd7014b9718a242cf27836
hbh7 <hbh7@hbh7.com>
2020-06-16 13:39:32 -0400

    Some config corrections, many lint fixes

--- PollBuddy-Server/backend/modules/mongoConnection.js
+++ PollBuddy-Server/backend/modules/mongoConnection.js
@@ -1,33 +1,33 @@
-const mongo = require('mongodb');
+const mongo = require("mongodb");
 const MongoClient = mongo.MongoClient;
 
 var db;
 
 module.exports = {
-	connect: function(callback){
+  connect: function(callback){
 
-		con();
+    con();
 
-		function con() {
-			const client = new MongoClient(process.env.DB_URL);
-			client.connect((err) => {
-				if (err) {
-					console.error("Seems the database isn't up yet, retrying in 1 second");
-					setTimeout(function () {
-						con();
-					}, 3000);
-				} else {
-					db = client.db(process.env.DB_NAME);
-					console.log('Database connected');
-				}
-			});
+    function con() {
+      const client = new MongoClient(process.env.DB_URL);
+      client.connect((err) => {
+        if (err) {
+          console.error("Seems the database isn't up yet, retrying in 1 second");
+          setTimeout(function () {
+            con();
+          }, 3000);
+        } else {
+          db = client.db(process.env.DB_NAME);
+          console.log("Database connected");
+        }
+      });
 
-		}
-	},
-	getDB: function(){
-		return db;
-	},
-	getMongo: function(){
-		return mongo;
-	}
+    }
+  },
+  getDB: function(){
+    return db;
+  },
+  getMongo: function(){
+    return mongo;
+  }
 };
--- PollBuddy-Server/backend/routes/users.js
+++ PollBuddy-Server/backend/routes/users.js
@@ -5,16 +5,16 @@ var mongoConnection = require("../modules/mongoConnection.js");
 var bcrypt = require("bcrypt");
 
 // GET users listing.
-router.get("/", function(req, res, next) {
-  mongoConnection.getDB().collection("users").find({}).toArray(function(err, result){
+router.get("/", function (req, res, next) {
+  mongoConnection.getDB().collection("users").find({}).toArray(function (err, result) {
     res.send(result);
   });
 });
-router.get("/login/", function(req, res, next) {
+router.get("/login/", function (req, res, next) {
   res.send("i am a login processor");//TODO
 });
 
-router.post("/register", function(req, res, next){
+router.post("/register", function (req, res, next) {
   var requestBody = req.body;
 
   mongoConnection.getDB().collection("users").insertOne({
@@ -26,129 +26,129 @@ router.post("/register", function(req, res, next){
   });
   return res.sendStatus(200);
 });
-router.post("/:id/edit/", function(req,res){//TODO RCS BOOL refer to documentation
+router.post("/:id/edit/", function (req, res) {//TODO RCS BOOL refer to documentation
   var id = new mongoConnection.getMongo().ObjectID(req.params.id);
   var jsonContent = req.body;
   var success = false;
-  if(jsonContent.Action === "Add") {
-    if(jsonContent.FirstName !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$addToSet":{FirstName: jsonContent.FirstName}}, function(err,res){
-        if(err){
+  if (jsonContent.Action === "Add") {
+    if (jsonContent.FirstName !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$addToSet": {FirstName: jsonContent.FirstName}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.LastName !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$addToSet":{LastName: jsonContent.LastName}}, function(err,res){
-        if(err){
+    if (jsonContent.LastName !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$addToSet": {LastName: jsonContent.LastName}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.Username !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$addToSet":{Username: jsonContent.Username}}, function(err,res){
-        if(err){
+    if (jsonContent.Username !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$addToSet": {Username: jsonContent.Username}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.Email !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$addToSet":{Email: jsonContent.Email}}, function(err,res){
-        if(err){
+    if (jsonContent.Email !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$addToSet": {Email: jsonContent.Email}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.Password !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$addToSet":{Password: bcrypt.hashSync(jsonContent.Password,10)}}, function(err,res){
-        if(err){
+    if (jsonContent.Password !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$addToSet": {Password: bcrypt.hashSync(jsonContent.Password, 10)}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(success === false){
+    if (success === false) {
       return res.sendStatus(400);
     }
-  } else if(jsonContent.Action === "Remove") {
-    if(jsonContent.FirstName !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$pull":{FirstName: jsonContent.FirstName}}, function(err,res){
-        if(err){
+  } else if (jsonContent.Action === "Remove") {
+    if (jsonContent.FirstName !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$pull": {FirstName: jsonContent.FirstName}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.LastName !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$pull":{LastName: jsonContent.LastName}}, function(err,res){
-        if(err){
+    if (jsonContent.LastName !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$pull": {LastName: jsonContent.LastName}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.Username !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$pull":{Username: jsonContent.Username}}, function(err,res){
-        if(err){
+    if (jsonContent.Username !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$pull": {Username: jsonContent.Username}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.Email !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$pull":{Email: jsonContent.Email}}, function(err,res){
-        if(err){
+    if (jsonContent.Email !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$pull": {Email: jsonContent.Email}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(jsonContent.Password !== undefined) {
-      mongoConnection.getDB().collection("users").updateOne({"_id" : id},{"$pull":{Password: jsonContent.Password}}, function(err,res){
-        if(err){
+    if (jsonContent.Password !== undefined) {
+      mongoConnection.getDB().collection("users").updateOne({"_id": id}, {"$pull": {Password: jsonContent.Password}}, function (err, res) {
+        if (err) {
           return res.sendStatus(500);
         } else {
           success = true;
         }
       });
     }
-    if(success === false){
+    if (success === false) {
       return res.sendStatus(400);
     }
-  }else {
+  } else {
     return res.sendStatus(400);
   }
   return res.sendStatus(200); // TODO: Ensure this is true
 });
-router.get("/:id/", function(req, res, next) {
+router.get("/:id/", function (req, res, next) {
   var id = new mongoConnection.getMongo().ObjectID(req.params.id);
-  mongoConnection.getDB().collection("users").find({"_id" : id}).toArray(function(err,result){
-    if(err){
+  mongoConnection.getDB().collection("users").find({"_id": id}).toArray(function (err, result) {
+    if (err) {
       return res.sendStatus(500);
     }
     return res.send(result);
   });
 });
 
-router.get("/:id/groups", function(req, res, next) {
+router.get("/:id/groups", function (req, res, next) {
   var id = new mongoConnection.getMongo().ObjectID(req.params.id);
-  mongoConnection.getDB().collection("users").find({"_id" : id},{projection:{_id: 0, Groups: 1}}).map(function(item){
+  mongoConnection.getDB().collection("users").find({"_id": id}, {projection: {_id: 0, Groups: 1}}).map(function (item) {
     return res.send(item.Groups);
-  }).toArray(function(err,result){
-    if(err){
+  }).toArray(function (err, result) {
+    if (err) {
       return res.sendStatus(500);
     }
     return res.send(result[0]);
