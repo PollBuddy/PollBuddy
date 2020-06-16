1dfb70c31a1e112693cca1e4f0c38609b2cf6b38
hbh7 <hbh7@hbh7.com>
2020-06-16 01:33:53 -0400

    Repaired eslint integration in package/package-lock.json, fixed tabs/spaces in pollcode.js

--- PollBuddy-Server/frontend/src/pages/pollCode/pollCode.js
+++ PollBuddy-Server/frontend/src/pages/pollCode/pollCode.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./pollCode.scss"
-import { MDBContainer } from "mdbreact";
+import {MDBContainer} from "mdbreact";
 import "mdbreact/dist/css/mdb.css";
 import logo from "../../images/logo.png";
 
 export default class pollCode extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.updateTitle("Poll Code");
   }
+
   render() {
     return (
       <MDBContainer>
-    		<MDBContainer fluid className="poll-homepage">
-          <img src={logo} className="img-fluid animated bounce infinite logo" alt="logo" />
+        <MDBContainer fluid className="poll-homepage">
+          <img src={logo} className="img-fluid animated bounce infinite logo" alt="logo"/>
 
-	    		<div className="form-group">
-			      <input type="email" className="form-control enterCode" placeholder="Poll Code" />
-			    </div>
-			    <button className = "btn poll-button">Join Poll</button>
-    		</MDBContainer>
+          <div className="form-group">
+            <input type="email" className="form-control enterCode" placeholder="Poll Code"/>
+          </div>
+          <button className="btn poll-button">Join Poll</button>
+        </MDBContainer>
       </MDBContainer>
     )
   }
