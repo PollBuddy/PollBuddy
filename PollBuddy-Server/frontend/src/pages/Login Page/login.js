8bcf24ca28b08f6ee75020cb23dc3ee055fb945d
hbh7 <hbh7@hbh7.com>
2020-06-19 17:48:58 -0400

    Frontend lint fixes after merge

--- PollBuddy-Server/frontend/src/pages/Login Page/login.js
+++ PollBuddy-Server/frontend/src/pages/Login Page/login.js
@@ -7,80 +7,80 @@ import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from "mdbreact";
 export default class login extends Component {
 
     state = {
-        successfulLogin: false
+      successfulLogin: false
     };
 
     constructor(){
-        super();
-        if(localStorage.getItem('loggedIn')){
-            this.setState({successfulLogin: true}) // Tell it to redirect to the next page if already logged in
-        }
+      super();
+      if(localStorage.getItem("loggedIn")){
+        this.setState({successfulLogin: true}) // Tell it to redirect to the next page if already logged in
+      }
     }
     handleLogin() {
-        //needs some authentication before and if authentication passes then set local storage and such refer to classcreation page to see the way to make POST requests to the backend
-        localStorage.setItem('loggedIn', true);//maybe have an admin/teacher var instead of just true
-        //TODO MAYBE IN THE FUTURE USE COOKIES TO REMEMBER PAST SESSION
-        this.setState({successfulLogin: true}) // Tell it to redirect to the next page if successful
+      //needs some authentication before and if authentication passes then set local storage and such refer to classcreation page to see the way to make POST requests to the backend
+      localStorage.setItem("loggedIn", true);//maybe have an admin/teacher var instead of just true
+      //TODO MAYBE IN THE FUTURE USE COOKIES TO REMEMBER PAST SESSION
+      this.setState({successfulLogin: true}) // Tell it to redirect to the next page if successful
     }
     componentDidMount(){
-        this.props.updateTitle("Log in");
+      this.props.updateTitle("Log in");
     }
     render() {
-        this.handleLogin = this.handleLogin.bind(this); // This is needed so stuff like this.setState works
+      this.handleLogin = this.handleLogin.bind(this); // This is needed so stuff like this.setState works
 
-        if(this.state.successfulLogin) { // Basically redirect if the person is logged in or if their login succeeds
-            return (
-               <Redirect to="/myClasses" />
-            )
-        }
+      if(this.state.successfulLogin) { // Basically redirect if the person is logged in or if their login succeeds
         return (
-            <MDBContainer className="page-login">
+          <Redirect to="/myClasses" />
+        )
+      }
+      return (
+        <MDBContainer className="page-login">
 
-        <MDBContainer className="d-flex p-2 Login-Box">
+          <MDBContainer className="d-flex p-2 Login-Box">
 
-          <MDBContainer>
-            <MDBRow>
-              <MDBCol md="6">
-                <form>
-                  <MDBContainer className="text_boxes">
-                    <MDBInput
-                      label="Type your email"
-                      icon="envelope"
-                      group
-                      type="email"
-                      validate
-                      error="wrong"
-                      success="right"
-                    />
-                    <MDBInput
-                      label="Type your password"
-                      icon="lock"
-                      group
-                      type="password"
-                      validate
-                    />
-                  </MDBContainer>
-                  <MDBContainer className="text-right">
-                    <Link to={"/forgotPassword"}>
-                      <MDBBtn size="lg" color="black" className="purple">Forgot Your Password</MDBBtn>
-                    </Link>
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol md="6">
+                  <form>
+                    <MDBContainer className="text_boxes">
+                      <MDBInput
+                        label="Type your email"
+                        icon="envelope"
+                        group
+                        type="email"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                      <MDBInput
+                        label="Type your password"
+                        icon="lock"
+                        group
+                        type="password"
+                        validate
+                      />
+                    </MDBContainer>
+                    <MDBContainer className="text-right">
+                      <Link to={"/forgotPassword"}>
+                        <MDBBtn size="lg" color="black" className="purple">Forgot Your Password</MDBBtn>
+                      </Link>
 
-                    <MDBBtn size="lg" color="black" className="purple" onClick={this.handleLogin} >Submit</MDBBtn>
-                  </MDBContainer>
+                      <MDBBtn size="lg" color="black" className="purple" onClick={this.handleLogin} >Submit</MDBBtn>
+                    </MDBContainer>
 
-                  <MDBContainer className="text-right">
-                    <Link to={"/registerDefault"}>
-                      <MDBBtn size="lg" color="black" className="sign_up">No account? Sign up</MDBBtn>
-                    </Link>
-                  </MDBContainer>
-                </form>
-              </MDBCol>
-            </MDBRow>
+                    <MDBContainer className="text-right">
+                      <Link to={"/registerDefault"}>
+                        <MDBBtn size="lg" color="black" className="sign_up">No account? Sign up</MDBBtn>
+                      </Link>
+                    </MDBContainer>
+                  </form>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+
           </MDBContainer>
 
         </MDBContainer>
-
-      </MDBContainer>
-    )
-  }
+      )
+    }
 }
--- PollBuddy-Server/frontend/src/pages/homepage/homepage.js
+++ PollBuddy-Server/frontend/src/pages/homepage/homepage.js
@@ -11,33 +11,33 @@ export default class homepage extends Component {
     this.props.updateTitle("Home");
   }
 
-    render() {
-        return (
-            <MDBContainer fluid className="page-homepage">
-                <img src={logo} alt="logo" className="logo img-fluid" />
+  render() {
+    return (
+      <MDBContainer fluid className="page-homepage">
+        <img src={logo} alt="logo" className="logo img-fluid" />
 
-                <p className = "blurb"> An interactive questionnaire platform made by students, for
+        <p className = "blurb"> An interactive questionnaire platform made by students, for
                     students, to strengthen lecture material and class attentiveness.</p>
-                <MDBContainer>
-                    <Link to={"/login"}>
-                        <button className = "btn button">Login</button>
-                    </Link>
-                    <Link to={"/registerDefault"}>
-                        <button className = "btn button">Register</button>
-                    </Link>
-                </MDBContainer>
+        <MDBContainer>
+          <Link to={"/login"}>
+            <button className = "btn button">Login</button>
+          </Link>
+          <Link to={"/registerDefault"}>
+            <button className = "btn button">Register</button>
+          </Link>
+        </MDBContainer>
 
-                <p className = "blurb2" > Already have a Poll Code? Enter it here.</p>
-                <MDBContainer className="form-group">
-                    <input placeholder="Poll Code" className="enterCode"/>
-                </MDBContainer>
-                <Link to={"/pollviewer"}>
-                    <button className = "btn poll-button">Join Poll</button>
-                </Link>
+        <p className = "blurb2" > Already have a Poll Code? Enter it here.</p>
+        <MDBContainer className="form-group">
+          <input placeholder="Poll Code" className="enterCode"/>
+        </MDBContainer>
+        <Link to={"/pollviewer"}>
+          <button className = "btn poll-button">Join Poll</button>
+        </Link>
 
-            </MDBContainer>
+      </MDBContainer>
 
 
-        )
-    }
+    )
+  }
 }
