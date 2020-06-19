cbd4550df775962397425d6a20e652a42d10febc
hbh7 <hbh7@hbh7.com>
2020-06-19 17:13:40 -0400

    Code style fixes after backend merge

--- PollBuddy-Server/backend/routes/users.js
+++ PollBuddy-Server/backend/routes/users.js
@@ -6,31 +6,36 @@ const bson = require("bson");
 var bcrypt = require("bcrypt");
 
 // Middleware for getting user information
-module.exports.user_middleware = function(req, res, next){
+module.exports.user_middleware = function (req, res, next) {
 
-	req.isLoggedIn = function(){
-		return req.session['UserID'] !== undefined;
-	}
+  req.isLoggedIn = function () {
+    return req.session["UserID"] !== undefined;
+  }
 
-	// If the current user is logged in, a user object will be returned, otherwise a 401 will be sent
-	// Callback takes two parameters: err and user
-	req.getCurrentUser = function(callback){
-		if(!req.isLoggedIn()){
-			res.status(401).send({
-				error: "Not logged in"
-			});
-			if(typeof callback == 'function') callback(new Error("Not logged in"));
-		}else{
-			mongoConnection.getDB().collection('users').findOne({_id:bson.ObjectId(req.session['UserID'])}, {projection: {Password: false}}, (err, result) => {
-				if(err) return callback(err);
-				else{
-					if(typeof callback == 'function') callback(null, result);
-				}
-			});
-		}
-	}
+  // If the current user is logged in, a user object will be returned, otherwise a 401 will be sent
+  // Callback takes two parameters: err and user
+  req.getCurrentUser = function (callback) {
+    if (!req.isLoggedIn()) {
+      res.status(401).send({
+        error: "Not logged in"
+      });
+      if (typeof callback === "function") {
+        callback(new Error("Not logged in"));
+      }
+    } else {
+      mongoConnection.getDB().collection("users").findOne({_id: bson.ObjectId(req.session["UserID"])}, {projection: {Password: false}}, (err, result) => {
+        if (err) {
+          return callback(err);
+        } else {
+          if (typeof callback === "function") {
+            callback(null, result);
+          }
+        }
+      });
+    }
+  }
 
-	next();
+  next();
 };
 
 // GET users listing.
@@ -39,65 +44,63 @@ router.get("/", function (req, res, next) {
     res.send(result);
   });
 });
-router.post('/login', function(req, res){
+router.post("/login", function (req, res) {
 
-	// Get email and password from request in JSON form
-	var email = req.body['email'];
-	var password = req.body['password'];
+  // Get email and password from request in JSON form
+  var email = req.body["email"];
+  var password = req.body["password"];
 
-	// Check if email is present
-	if(email === undefined){
-		res.status(400).send({
-			error: "Missing email"
-		});
-	}
-	// Check if password is present
-	else if(password === undefined){
-		res.status(400).send({
-			error: "Missing password"
-		});
-	}
-
-	else{
-		mongoConnection.getDB().collection('users').findOne({Email:email}, {_id: true, Password: true}, (err_db, result_db) => {
-			if(err_db){
-				console.error(err_db);
-				res.status(500).send({
-					error: "Database error"
-				});
-			}
-			else if(result_db === null){
-				res.status(401).send({
-					error: "Invalid credentials"
-				});
-			}else{
-				bcrypt.compare(password, result_db['Password'], (err_compare, result_compare) => {
-					if(err_compare){
-						console.error(err_compare);
-						res.status(500).send({
-							error: "Hash comparison error"
-						});
-					}else if(result_compare){
-						req.session.regenerate((err_regen) => {
-							if(err_regen){
-								console.error(err_regen);
-								res.status(500).send({
-									error: "Error regenerating session"
-								});
-							}else{
-								req.session['UserID'] = result_db['_id'];
-								res.sendStatus(200);
-							}
-						});
-					}else{
-						res.status(401).send({
-							error: "Invalid credentials"
-						});
-					}
-				});
-			}
-		});
-	}
+  // Check if email is present
+  if (email === undefined) {
+    res.status(400).send({
+      error: "Missing email"
+    });
+  } else if (password === undefined) { // Check if password is present
+    res.status(400).send({
+      error: "Missing password"
+    });
+  } else {
+    mongoConnection.getDB().collection("users").findOne({Email: email}, {
+      _id: true,
+      Password: true
+    }, (err_db, result_db) => {
+      if (err_db) {
+        console.error(err_db);
+        res.status(500).send({
+          error: "Database error"
+        });
+      } else if (result_db === null) {
+        res.status(401).send({
+          error: "Invalid credentials"
+        });
+      } else {
+        bcrypt.compare(password, result_db["Password"], (err_compare, result_compare) => {
+          if (err_compare) {
+            console.error(err_compare);
+            res.status(500).send({
+              error: "Hash comparison error"
+            });
+          } else if (result_compare) {
+            req.session.regenerate((err_regen) => {
+              if (err_regen) {
+                console.error(err_regen);
+                res.status(500).send({
+                  error: "Error regenerating session"
+                });
+              } else {
+                req.session["UserID"] = result_db["_id"];
+                res.sendStatus(200);
+              }
+            });
+          } else {
+            res.status(401).send({
+              error: "Invalid credentials"
+            });
+          }
+        });
+      }
+    });
+  }
 
 });
 
