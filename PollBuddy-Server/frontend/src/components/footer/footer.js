20cc33a231a8bd169df288fddb1d43bd093b6398
Annie Shi <annieshi6@gmail.com>
2020-06-26 07:07:47 -0400

    redone footer to include copy 2020 poll buddy, center links

--- PollBuddy-Server/frontend/src/components/footer/footer.js
+++ PollBuddy-Server/frontend/src/components/footer/footer.js
@@ -10,27 +10,32 @@ export default class Footer extends Component {
     return (
       <footer className = "footer_foot">
         <MDBContainer className = "footer_linethru" />
-        <MDBContainer className = "footer_logo_links">
-          <a href = "https://rcos.io/" target = "_blank" rel="noopener noreferrer">
-            <img src = {rcosLogo} alt = "RCOS" />
-          </a>
-          <a href = "https://github.com/PollBuddy/PollBuddy" target = "_blank" rel="noopener noreferrer">
-            <img src = {githubLogo} alt = "Github" />
-          </a>
-        </MDBContainer>
-        <MDBContainer className = "footer_foot_links">
-          <a href = "/">
-						About
-          </a>
-          <a href = "https://info.rpi.edu/statement-of-accessibility" target = "_blank" rel = "noopener noreferrer">
-						Accessibility
-          </a>
-          <a href = "mailto:contactus@pollbuddy.app">
-						Contact
-          </a>
-          <a href = "/privacy">
-						Privacy
-          </a>
+        <MDBContainer className = "footer_links">
+          <MDBContainer className = "footer_foot_links">
+            &copy; 2020 Poll Buddy
+          </MDBContainer>
+          <MDBContainer className = "footer_foot_links">
+            <a href = "/">
+              About
+            </a>
+            <a href = "https://info.rpi.edu/statement-of-accessibility" target = "_blank" rel = "noopener noreferrer">
+              Accessibility
+            </a>
+            <a href = "mailto:contactus@pollbuddy.app">
+              Contact
+            </a>
+            <a href = "/privacy">
+              Privacy
+            </a>
+          </MDBContainer>
+          <MDBContainer className = "footer_foot_links">
+            <a href = "https://rcos.io/" target = "_blank" rel="noopener noreferrer">
+              <img src = {rcosLogo} alt = "RCOS" />
+            </a>
+            <a href = "https://github.com/PollBuddy/PollBuddy" target = "_blank" rel="noopener noreferrer">
+              <img src = {githubLogo} alt = "Github" />
+            </a>
+          </MDBContainer>
         </MDBContainer>
       </footer>
     )
