2e6430df76cea64abe7c977984d7f332ddb0b463
schans2 <schantz.samuel@gmail.com>
2020-07-10 13:49:49 -0400

    Added unique key identifier to remove warning
    Sorry this is still a mess, I'm trying, thanks for patience :)

--- PollBuddy-Server/frontend/src/pages/registerWithSchool/registerWithSchool.js
+++ PollBuddy-Server/frontend/src/pages/registerWithSchool/registerWithSchool.js
@@ -19,32 +19,42 @@ export default class registerWithSchool extends Component {
           <p className="width-90 fontSizeSmall">
             To create an account, enter your school name or login using RPI's CAS.
           </p>
-          <p className="width-90 fontSizeSmall">
+          <p className="width-90 fontSizeSmall" id="schoolNameText">
             School Name:
           </p>
           <MDBContainer className="form-group">
             <Autocomplete
               getItemValue={(item) => item.label}
               items={[
-                { label: "Rensselaer Polytechnic Institute" },
-                { label: "Worcester Polytechnic Institute" },
-                { label: "Massachusetts Institute of Technology" },
-                { label: "Rochester Institute of Technology" },
-                { label: "University of Rochester" },
-                { label: "SUNY Polytechnic Institute" },
-                { label: "SUNY Albany" },
-                { label: "Albany Medical College" }
+                { key: 0, label: "Rensselaer Polytechnic Institute" },
+                { key: 1, label: "Worcester Polytechnic Institute" },
+                { key: 2, label: "Massachusetts Institute of Technology" },
+                { key: 3, label: "Rochester Institute of Technology" },
+                { key: 4, label: "University of Rochester" },
+                { key: 5, label: "SUNY Polytechnic Institute" },
+                { key: 6, label: "SUNY Albany" },
+                { key: 7, label: "Albany Medical College" }
               ]}
+              /*
               renderInput={function(props) {
-                return <input placeholder="Enter school name" className="form-control textBox"/>
+                return (<input
+                  aria-labelledby="schoolNameText"
+                  placeholder="Enter school name"
+                  className="form-control textBox"
+                  onClick={props.onClick(props.ref)}
+                  onChange={props.onChange(props.ref)}
+                />)
               }}
+              */
               renderItem={(item, isHighlighted) =>
-                <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                <div key={item.key} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
                   {item.label}
                 </div>
               }
-              // value={value}
-              // onChange={(e) => value = e.target.value}
+              value={""}
+              onChange={(e, value) => value = e.target.value}
+              // onChange={(e, value) => {console.log(value); console.log(e.target.value)}}
+              // these are one and the same
               // onSelect={(val) => value = val}
             />
           </MDBContainer>
@@ -55,11 +65,10 @@ export default class registerWithSchool extends Component {
 
           <form>
             <button className="btn button"
-              formAction="https://cas-auth.rpi.edu/cas/login?service=http%3A%2F%2Fcms.union.rpi.edu%2Flogin%2Fcas%2F%3Fnext%3Dhttps%253A%252F%252Fwww.google.com%252F">CAS
-              (I'm an RPI student)
+              formAction="https://cas-auth.rpi.edu/cas/login?service=http%3A%2F%2Fcms.union.rpi.edu%2Flogin%2Fcas%2F%3Fnext%3Dhttps%253A%252F%252Fwww.google.com%252F">
+              CAS (I'm an RPI student)
             </button>
           </form>
-
 
         </MDBContainer>
       </MDBContainer>
