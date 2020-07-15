b4efeceb7a1607bfebfaf922a00a7510f3b521b8
Andrew <emanutter@gmail.com>
2020-07-15 15:15:06 -0400

    About page now displays text from markdown file

--- PollBuddy-Server/frontend/src/pages/about/about.js
+++ PollBuddy-Server/frontend/src/pages/about/about.js
@@ -1,8 +1,22 @@
 import React, {Component} from "react";
 import "mdbreact/dist/css/mdb.css";
 import {MDBContainer} from "mdbreact";
+import ReactMarkdown from "react-markdown";
+import aboutMdPath from "./about.md";
 
 export default class about extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {terms: null}
+  }
+
+  componentWillMount() {
+    fetch(aboutMdPath).then((response) => response.text()).then((text) => {
+      this.setState({terms: text})
+    })
+  }
+
   componentDidMount() {
     this.props.updateTitle("About");
   }
@@ -10,7 +24,10 @@ export default class about extends Component {
   render() {
     return (
       <MDBContainer className="page">
-        
+        <p className="fontSizeSmall width=90">
+          {/* Render page from markdown file using react-markdown */}
+          <ReactMarkdown source={this.state.terms} />         
+        </p>
       </MDBContainer>
     )
   }
---
+++ PollBuddy-Server/frontend/src/pages/about/about.md
@@ -0,0 +1,9 @@
+About
+
+Page
+
+Text
+
+Goes
+
+Here
