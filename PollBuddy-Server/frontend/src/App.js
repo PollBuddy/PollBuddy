d51c5bc2de7bcacffe417a6136713d02a0aef527
hbh7 <hbh7@hbh7.com>
2020-07-23 01:39:58 -0400

    A few more semicolons

--- PollBuddy-Server/frontend/src/App.js
+++ PollBuddy-Server/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom"
-import {MDBContainer} from "mdbreact"
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {MDBContainer} from "mdbreact";
 
 import Myclasses from "./pages/myclasses/myclasses";
 import Homepage from "./pages/homepage/homepage";
