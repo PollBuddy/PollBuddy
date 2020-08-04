3ffa7326cb1569f53b75a6090459bd327edcc5de
hbh7 <hbh7@hbh7.com>
2020-08-03 22:12:39 -0400

    Lint fix

--- PollBuddy-Server/backend/modules/influx.js
+++ PollBuddy-Server/backend/modules/influx.js
@@ -1,4 +1,4 @@
-const Influx = require('influx');
+const Influx = require("influx");
 let influx = new Influx.InfluxDB({
   host: "influxdb", // You must run in Docker-Compose for this to work
   database: "pollbuddy",
@@ -10,7 +10,7 @@ module.exports = {
 
   log: function (data) {
     influx.writePoints(data).catch(err => {
-      console.error(`Error saving data to InfluxDB! ${err.stack}`)
+      console.error(`Error saving data to InfluxDB! ${err.stack}`);
     });
   }
 
