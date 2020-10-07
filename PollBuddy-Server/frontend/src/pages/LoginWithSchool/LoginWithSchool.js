744e4d6dc0c3f2756bb1aa5ec398e1d2047f2f6a
Edvinas <edvinas.simkus@skyscanner.net>
2020-10-07 08:44:00 +0100

    Fix styling issues in login with school page
    This commit shoud fix the autoselect dropdown styling issues in login
    with school page. It also extracts that dropdown to a new SchoolPicker
    component because it is used in (at least) 2 places

--- PollBuddy-Server/frontend/src/pages/LoginWithSchool/LoginWithSchool.js
+++ PollBuddy-Server/frontend/src/pages/LoginWithSchool/LoginWithSchool.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
-import Autocomplete from "react-autocomplete";
 import {MDBContainer} from "mdbreact";
 import "mdbreact/dist/css/mdb.css";
 import {Link} from "react-router-dom";
+
+import SchoolPicker from "../../components/SchoolPicker/SchoolPicker"
 
 export default class registerWithSchool extends Component {
   componentDidMount() {
@@ -27,55 +28,12 @@ export default class registerWithSchool extends Component {
           <p className="width-90 fontSizeSmall" id="schoolNameText">
             School Name:
           </p>
-          <MDBContainer className="form-group" style={{ width: "100%" }}>
-            <Autocomplete
-              items={[
-                { key: 0, label: "Rensselaer Polytechnic Institute" },
-                { key: 1, label: "Worcester Polytechnic Institute" },
-                { key: 2, label: "Massachusetts Institute of Technology" },
-                { key: 3, label: "Rochester Institute of Technology" },
-                { key: 4, label: "University of Rochester" },
-                { key: 5, label: "SUNY Polytechnic Institute" },
-                { key: 6, label: "SUNY Albany" },
-                { key: 7, label: "Albany Medical College" }
-              ]}
-              getItemValue={item => item.label}
-              sortItems={(itemA, itemB, value) => {
-                const lowA = itemA.label.toLowerCase();
-                const lowB = itemB.label.toLowerCase();
-                const indexA = lowA.indexOf(value.toLowerCase());
-                const indexB = lowB.indexOf(value.toLowerCase());
-                if(indexA !== indexB) {
-                  return (indexA - indexB);
-                }
-                return (lowA < lowB ? -1 : 1);
-              }}
-              shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) >= 0}
-              renderItem={(item, isHighlighted) =>
-                <div
-                  key={item.key}
-                  className="fontSizeSmall"
-                  style={{
-                    background: isHighlighted ? "#DFCFEA" : "#FFF",
-                    fontFamily: "monospace",
-                    textAlign: "center"
-                  }}
-                >
-                  {item.label}
-                </div>
-              }
-              inputProps={{
-                className: "form-control textBox",
-                style: { width: "100%" },
-                placeholder: "Enter school name",
-                "aria-labelledby": "schoolNameText"
-              }}
-              wrapperStyle={{ display: "inline-block", width: "100%"}}
-              value={this.state.value}
-              onChange={e => this.setState({ value: e.target.value })}
-              onSelect={value => this.setState({ value })}
-            />
-          </MDBContainer>
+
+          <SchoolPicker 
+            value={this.state.value}
+            onChange={e => this.setState({ value: e.target.value })}
+            onSelect={value => this.setState({ value })}
+          />
 
           <Link to={"/accountinfo"}>
             <button className="btn button">Submit</button>
