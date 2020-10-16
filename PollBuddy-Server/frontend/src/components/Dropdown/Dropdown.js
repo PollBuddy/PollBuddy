0365d456d3bf7fa7ba417c1c5bc3476f55a92768
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-10-16 18:05:36 -0400

    Added links to header dropdown menu items
    Also addressed some lint errors.

--- PollBuddy-Server/frontend/src/components/Dropdown/Dropdown.js
+++ PollBuddy-Server/frontend/src/components/Dropdown/Dropdown.js
@@ -1,47 +1,49 @@
 import React, { Component, useState } from "react";
 import "mdbreact/dist/css/mdb.css";
 import "./Dropdown.scss";
+import "../Header/Header.scss";
 
 export default class Dropdown extends Component {
-    render() {
-        return (
-            <DropdownButton>
-                <DropdownMenu></DropdownMenu>
-            </DropdownButton>
-        );
-    }
+  render() {
+    return (
+      <DropdownButton>
+        <DropdownMenu></DropdownMenu>
+      </DropdownButton>
+    );
+  }
 }
 
 function DropdownButton(props) {
-    const [open, setOpen] = useState(false);
-    return (
-        <span onClick={() => setOpen(!open)}>
-            <span className="header_bar_btn">Menu</span>
+  const [open, setOpen] = useState(false);
+  return (
+    <span onClick={() => setOpen(!open)}>
+      <span className="header_bar_btn">Menu</span>
 
-            {open && props.children}
-        </span>
-    );
+      {open && props.children}
+    </span>
+  );
 }
 
 function DropdownMenu() {
 
-    function DropdownItem(props) {
-        return (
-            <a href="#" className="menu-item">
-                {props.children}
-            </a>
-        );
-    }
-
+  function DropdownItem(props) {
     return (
-        <div className="dropdown">
-            <DropdownItem>Login</DropdownItem>
-            <DropdownItem>Logout</DropdownItem>
-            <DropdownItem>Register</DropdownItem>
-            <DropdownItem>Account</DropdownItem>
-            <DropdownItem>Enter Poll Code</DropdownItem>
-            <DropdownItem>Groups</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-        </div>
+      <a href={props.link} className="dropdown_menu_item">
+        {props.children}
+      </a>
     );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem link="/login">Login</DropdownItem>
+      <DropdownItem link="/">Logout</DropdownItem>
+      <DropdownItem link="/register">Register</DropdownItem>
+      <DropdownItem link="/account">Account</DropdownItem>
+      <DropdownItem link="/poll/:pollID/view">Enter Poll Code</DropdownItem>
+      <DropdownItem link="/groups">Groups</DropdownItem>
+      <DropdownItem link="/">History</DropdownItem>
+      <DropdownItem link="/">Settings</DropdownItem>
+    </div>
+  );
 }
