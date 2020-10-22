2b50999095823a4ea3e572d6eeb0980cd54d4f13
Yao Dong <18ydong@gmail.com>
2020-10-22 18:53:23 -0400

    Reviewed changes

--- PollBuddy-Server/frontend/src/pages/PollHistory/PollHistory.js
+++ PollBuddy-Server/frontend/src/pages/PollHistory/PollHistory.js
@@ -6,20 +6,7 @@ import {MDBContainer} from "mdbreact";
 //TODO Need to dynamically grab what polls the user has access to from the backend
 //TODO Need a way to know which polls the users has admin access to and which they do not
 
-// /api/users/
 export default class Groups extends Component {
-  constructor() {
-    super();
-    if (!localStorage.getItem("loggedIn")) {
-      //Redirect("/login");//this is a way to redirect the user to the page
-      //window.location.reload(false);//this forces a reload so this will make the user go to the login page. A little barbaric but it works. If frontend wants to make it better by all means
-    }
-  }
-
-  signout() {
-    //localStorage.removeItem("loggedIn");//todo if admin -- more specifically make diff states if the user who logged in is an admin... or teacher. wouldn't want teacher accessing user things or vice versa...
-    //Redirect("/login");
-  }
 
   componentDidMount() {
     this.props.updateTitle("My Poll Histories");
