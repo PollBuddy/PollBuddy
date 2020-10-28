2ab0307cbed4546c0cba531429d81f6757ca026a
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-10-27 23:50:53 -0400

    Close menu upon clicking outside it

--- PollBuddy-Server/frontend/src/components/Dropdown/Dropdown.js
+++ PollBuddy-Server/frontend/src/components/Dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useRef, useEffect } from "react";
 import "mdbreact/dist/css/mdb.css";
 import "./Dropdown.scss";
 import "../Header/Header.scss";
@@ -6,25 +6,45 @@ import "../Header/Header.scss";
 export default class Dropdown extends Component {
   render() {
     return (
-      <DropdownButton>
-        <DropdownMenu></DropdownMenu>
-      </DropdownButton>
+      <DropdownButton></DropdownButton>
     );
   }
 }
 
-function DropdownButton(props) {
+function DropdownButton() {
   const [open, setOpen] = useState(false);
+  function handleStateChange() {
+    setOpen(!open);
+  }
   return (
     <span onClick={() => setOpen(!open)}>
       <span className="header_bar_btn">Menu</span>
 
-      {open && props.children}
+      {open && <DropdownMenu onStateChange={handleStateChange}></DropdownMenu>}
     </span>
   );
 }
 
-function DropdownMenu() {
+function useOutsideAlerter(ref, menuProps) {
+  useEffect(() => {
+    // Close menu if click outside
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        menuProps.onStateChange();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+function DropdownMenu(props) {
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, props);
 
   function DropdownItem(props) {
     return (
@@ -35,6 +55,7 @@ function DropdownMenu() {
   }
 
   return (
+    <div ref={wrapperRef}>
     <div className="dropdown">
       <DropdownItem link="/login">Login</DropdownItem>
       <DropdownItem link="/">Logout</DropdownItem>
@@ -44,6 +65,7 @@ function DropdownMenu() {
       <DropdownItem link="/groups">Groups</DropdownItem>
       <DropdownItem link="/">History</DropdownItem>
       <DropdownItem link="/">Settings</DropdownItem>
+    </div>
     </div>
   );
 }
