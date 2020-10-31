f2f3406365017855da101c5eca393f2c93ba6ff4
Neha Deshpande <neha.deshpande001@gmail.com>
2020-10-31 02:00:43 -0400

    dynamically renders class button based on state arrays

--- PollBuddy-Server/frontend/src/pages/Groups/Groups.js
+++ PollBuddy-Server/frontend/src/pages/Groups/Groups.js
@@ -5,11 +5,24 @@ import { MDBContainer } from "mdbreact";
 export default class Groups extends Component {
   constructor(){
     super();
+    this.state = {
+      admin_classes: [
+        {key: 0, id: 123, label: "CSCI 1200 - Data Structures"},
+        {key: 1, id: 123, label: "CSCI 2200 - FOCS"}
+      ],
+      member_classes: [
+        {key: 0, id: 123, label: "CSCI 2300 - Intro to Algorithms"},
+        {key: 1, id: 123, label: "CSCI 2500 - Computer Organization"},
+        {key: 2, id: 123, label: "CSCI 2960 - RCOS"}
+      ]
+    };
+    
     if(!localStorage.getItem("loggedIn")){
       //Redirect("/login");//this is a way to redirect the user to the page
       //window.location.reload(false);//this forces a reload so this will make the user go to the login page. A little barbaric but it works. If frontend wants to make it better by all means
     }
   }
+
   signout(){
     //localStorage.removeItem("loggedIn");//todo if admin -- more specifically make diff states if the user who logged in is an admin... or teacher. wouldn't want teacher accessing user things or vice versa...
     //Redirect("/login");
@@ -17,7 +30,7 @@ export default class Groups extends Component {
   componentDidMount(){
     this.props.updateTitle("My Groups");
   }
-  render() { 
+  render() {
     return (
 
       <MDBContainer className="page">
@@ -25,25 +38,20 @@ export default class Groups extends Component {
           <p className="fontSizeLarge">
             As a Group Admin:
           </p>
-          <Link to={"/groups/123/edit"}>
-            <button className="btn button">CSCI 1200 - Data Structures</button>
-          </Link>
-          <Link to={"/groups/123/edit"}>
-            <button className="btn button">CSCI 2200 - Foundations of Computer Science</button>
-          </Link>
+          {this.state.admin_classes.map((e) => (
+              <Link to={"/groups/" + e.id + "/polls"}>
+                <button className="btn button">{e.label}</button>
+              </Link>
+          ))}
 
           <p className="fontSizeLarge">
             As a Group Member:
           </p>
-          <Link to={"/groups/123/polls"}>
-            <button className="btn button">CSCI 2300 - Intro to Algorithms</button>
-          </Link>
-          <Link to={"/groups/123/polls"}>
-            <button className="btn button">CSCI 2500 - Computer Organization</button>
-          </Link>
-          <Link to={"/groups/123/polls"}>
-            <button className="btn button">CSCI 2960 - RCOS</button>
-          </Link>
+          {this.state.member_classes.map((e) => (
+              <Link to={"/groups/" + e.id + "/polls"}>
+                <button className="btn button">{e.label}</button>
+              </Link>
+          ))}
 
           <p className="fontSizeLarge">
             Group Management:
