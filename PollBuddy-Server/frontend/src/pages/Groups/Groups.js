345f95bebf459c102dfb7e70439a22cb4a59cce8
Neha Deshpande <neha.deshpande001@gmail.com>
2020-10-31 02:27:18 -0400

    make group buttons the same size

--- PollBuddy-Server/frontend/src/pages/Groups/Groups.js
+++ PollBuddy-Server/frontend/src/pages/Groups/Groups.js
@@ -2,21 +2,30 @@ import React, {Component} from "react";
 import {Link, Redirect} from "react-router-dom";
 import { MDBContainer } from "mdbreact";
 
+function getGroups(){
+  return [
+    {key: 0, id: 123, label: "CSCI 2300 - Intro to Algorithms"},
+    {key: 1, id: 123, label: "CSCI 2500 - Computer Organization"},
+    {key: 2, id: 123, label: "CSCI 2960 - RCOS"}
+  ]
+}
+
 export default class Groups extends Component {
   constructor(){
     super();
     this.state = {
-      admin_classes: [
+      admin_groups: [
         {key: 0, id: 123, label: "CSCI 1200 - Data Structures"},
-        {key: 1, id: 123, label: "CSCI 2200 - FOCS"}
+        {key: 1, id: 123, label: "CSCI 2200 - Foundations of Computer Science"}
       ],
-      member_classes: [
+      member_groups: [
         {key: 0, id: 123, label: "CSCI 2300 - Intro to Algorithms"},
         {key: 1, id: 123, label: "CSCI 2500 - Computer Organization"},
         {key: 2, id: 123, label: "CSCI 2960 - RCOS"}
-      ]
+      ],
+      all_groups: getGroups()
     };
-    
+
     if(!localStorage.getItem("loggedIn")){
       //Redirect("/login");//this is a way to redirect the user to the page
       //window.location.reload(false);//this forces a reload so this will make the user go to the login page. A little barbaric but it works. If frontend wants to make it better by all means
@@ -38,18 +47,18 @@ export default class Groups extends Component {
           <p className="fontSizeLarge">
             As a Group Admin:
           </p>
-          {this.state.admin_classes.map((e) => (
+          {this.state.admin_groups.map((e) => (
               <Link to={"/groups/" + e.id + "/polls"}>
-                <button className="btn button">{e.label}</button>
+                <button className="btn button width-20em">{e.label}</button>
               </Link>
           ))}
 
           <p className="fontSizeLarge">
             As a Group Member:
           </p>
-          {this.state.member_classes.map((e) => (
+          {this.state.member_groups.map((e) => (
               <Link to={"/groups/" + e.id + "/polls"}>
-                <button className="btn button">{e.label}</button>
+                <button className="btn button width-20em">{e.label}</button>
               </Link>
           ))}
 
