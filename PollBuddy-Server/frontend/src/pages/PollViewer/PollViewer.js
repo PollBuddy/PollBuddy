26a4a60416b254c63501bdfe614eabbb7e54e3e4
Yao Dong <18ydong@gmail.com>
2020-11-08 19:51:39 -0500

    Temporary solution

--- PollBuddy-Server/frontend/src/pages/PollViewer/PollViewer.js
+++ PollBuddy-Server/frontend/src/pages/PollViewer/PollViewer.js
@@ -20,11 +20,12 @@ export default class PollViewer extends Component {
             </p>
             
             <ul>
-              <li id="answerElement0"><a href={"#1"}><span className={"PollViewer-bubble"}>A</span>She grinds them into the fairy dust she needs to fly</a></li>
-              <li id="answerElement1"><a href={"#2"}><span className={"PollViewer-bubble"}>B</span>She gives them to new babies who are ready to grow teeth</a></li>
-              <li id="answerElement2"><a href={"#3"}><span className={"PollViewer-bubble"}>C</span>She gives the good teeth to dentists to make false teeth</a></li>
-              <li id="answerElement3"><a href={"#4"}><span className={"PollViewer-bubble"}>D</span>She grinds them up and makes sand for the beach</a></li>
-              <li id="answerElement4"><a href={"#5"}><span className={"PollViewer-bubble"}>E</span>She needs to replace her own teeth</a></li>
+
+              <li id="answerElement0"><a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>A</span>She grinds them into the fairy dust she needs to fly</a></li>
+              <li id="answerElement1"><a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>B</span>She gives them to new babies who are ready to grow teeth</a></li>
+              <li id="answerElement2"><a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>C</span>She gives the good teeth to dentists to make false teeth</a></li>
+              <li id="answerElement3"><a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>D</span>She grinds them up and makes sand for the beach</a></li>
+              <li id="answerElement4"><a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>E</span>She needs to replace her own teeth</a></li>
             </ul>
 
           </MDBContainer>
