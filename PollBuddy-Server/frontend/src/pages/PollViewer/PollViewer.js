71705174c95ad825bbab97bf1d69a432d82fc97f
Yao Dong <18ydong@gmail.com>
2020-11-22 12:18:06 -0500

    Merging of the three branches is complete, but timer is weird with state. State changes rerenders the timer, causing the timer to reset.

--- PollBuddy-Server/frontend/src/pages/PollViewer/PollViewer.js
+++ PollBuddy-Server/frontend/src/pages/PollViewer/PollViewer.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import "mdbreact/dist/css/mdb.css";
 import "./PollViewer.scss";
-import { MDBContainer } from "mdbreact";
+import {MDBContainer} from "mdbreact";
 import Countdown, {zeroPad} from "react-countdown";
 import {Link} from "react-router-dom";
 
@@ -13,68 +13,64 @@ export default class PollViewer extends Component {
   timeLimit = 5;
 
   state = {
-    display: false
+    //use this state to check if the answer has been recorded
+    recorded: false
   }
 
   pollViewer() {
     return (
-      <MDBContainer>
-        <MDBContainer className="page">
-          <MDBContainer className="box PollViewer-answers">
-            <p>
-              Question 3 of 28:
-            </p>
-            <p className="fontSizeLarge">
-              Why does the tooth fairy collect teeth?
-            </p>
+      <>
+        <MDBContainer className="box PollViewer-answers">
+          <p>
+            Question 3 of 28:
+          </p>
+          <p className="fontSizeLarge">
+            Why does the tooth fairy collect teeth?
+          </p>
 
-            <ul>
+          <ul>
 
-              <li id="answerElement0">
-                <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>A</span>She grinds them into the fairy
-                  dust she needs to fly
-                </a>
-              </li>
-              <li id="answerElement1">
-                <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>B</span>She gives them to new babies
-                  who are ready to grow teeth
-                </a>
-              </li>
-              <li id="answerElement2">
-                <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>C</span>She gives the good teeth to
-                  dentists to make false teeth
-                </a>
-              </li>
-              <li id="answerElement3">
-                <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>D</span>She grinds them up and makes
-                  sand for the beach
-                </a>
-              </li>
-              <li id="answerElement4">
-                <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>E</span>She needs to replace her own
-                  teeth
-                </a>
-              </li>
-            </ul>
+            <li id="answerElement0">
+              <a onClick={() =>this.setState({recorded: true})}><span className={"PollViewer-bubble"}>A</span>She grinds them into the fairy
+                dust she needs to fly
+              </a>
+            </li>
+            <li id="answerElement1">
+              <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>B</span>She gives them to new babies
+                who are ready to grow teeth
+              </a>
+            </li>
+            <li id="answerElement2">
+              <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>C</span>She gives the good teeth to
+                dentists to make false teeth
+              </a>
+            </li>
+            <li id="answerElement3">
+              <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>D</span>She grinds them up and makes
+                sand for the beach
+              </a>
+            </li>
+            <li id="answerElement4">
+              <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>E</span>She needs to replace her own
+                teeth
+              </a>
+            </li>
+          </ul>
 
-          </MDBContainer>
         </MDBContainer>
-      </MDBContainer>
+      </>
     );
   }
 
   answerRecorded() {
     return (
-      <MDBContainer fluid className="page">
-        <button className="btn button" onClick={this.setState({display: 0})}>Change Answer?</button>
+      <>
+        <button className="btn button" onClick={() => this.setState({recorded: false})}>Change Answer?</button>
         <Link to={"/myclasses"}>
           <button className="btn button">Leave Poll?</button>
         </Link>
-
         <p className="width-45 fontSizeSmall">Waiting for next question...</p>
-
-
-      </MDBContainer>
+      </>
     );
   }
 
@@ -102,13 +98,14 @@ export default class PollViewer extends Component {
 
 
   render() {
+    //timer: if timer is complete, return questionEnded, else returns a timer
     const clockFormat = ({minutes, seconds, completed}) => {
       if (completed) {
         // Render a completed state
         return this.questionEnded();
       } else {
         // Render a countdown
-        return(
+        return (
           <>
             <p className="width-90 fontSizeLarge">
               Time remaining:
@@ -120,67 +117,21 @@ export default class PollViewer extends Component {
         );
       }
     };
-    let timer = (
-      <MDBContainer fluid className="page">
-        <p className="width-90 fontSizeLarge">
-          Time remaining:
-        </p>
-        <Countdown renderer={clockFormat} date={Date.now() + this.timeLimit * 1000}/>
+    let display;
+    if (this.state.recorded) {
+      display = this.answerRecorded()
+    } else {
+      display = this.pollViewer()
+    }
+    return (
+      <MDBContainer>
+        <MDBContainer className="page">
+          //renders a timer
+          <Countdown renderer={clockFormat} date={Date.now() + this.timeLimit * 1000}/>
+          //use display to choose between pollViewer and answerRecorded
+          {display}
+        </MDBContainer>
       </MDBContainer>
     );
-    if (this.state.display) {
-      return (
-        <React.Fragment>
-          timer,
-          this.answerRecorded()
-        </React.Fragment>
-      );
-    } else {
-      return(
-        <MDBContainer>
-          <MDBContainer className="page">
-            <Countdown renderer={clockFormat} date={Date.now() + this.timeLimit * 1000}/>
-            <MDBContainer className="box PollViewer-answers">
-              <p>
-                Question 3 of 28:
-              </p>
-              <p className="fontSizeLarge">
-                Why does the tooth fairy collect teeth?
-              </p>
-
-              <ul>
-
-                <li id="answerElement0">
-                  <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>A</span>She grinds them into the fairy
-                    dust she needs to fly
-                  </a>
-                </li>
-                <li id="answerElement1">
-                  <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>B</span>She gives them to new babies
-                    who are ready to grow teeth
-                  </a>
-                </li>
-                <li id="answerElement2">
-                  <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>C</span>She gives the good teeth to
-                    dentists to make false teeth
-                  </a>
-                </li>
-                <li id="answerElement3">
-                  <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>D</span>She grinds them up and makes
-                    sand for the beach
-                  </a>
-                </li>
-                <li id="answerElement4">
-                  <a href={"/answerRecorded"}><span className={"PollViewer-bubble"}>E</span>She needs to replace her own
-                    teeth
-                  </a>
-                </li>
-              </ul>
-
-            </MDBContainer>
-          </MDBContainer>
-        </MDBContainer>
-      );
-    }
   }
 }
