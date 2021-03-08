d16dea96bdb7b4470062c3fce401fc468fe17cab
Saptarshi Ganguly <gangus@rpi.edu>
2021-03-07 19:22:24 -0500

    Account info frontend changes
    Made significant progress on the front end of the account information page
    Spread the input boxes out and disabled the username box from being edited
    Ensured mobile compatibility (i think)
    Links don't do anything as of now

--- PollBuddy-Server/frontend/src/pages/AccountInfo/AccountInfo.js
+++ PollBuddy-Server/frontend/src/pages/AccountInfo/AccountInfo.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "mdbreact/dist/css/mdb.css";
-import { MDBContainer } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import {Link} from "react-router-dom";
 
 
@@ -14,27 +14,53 @@ export default class AccountInfo extends Component {
       <MDBContainer className="page">
         <MDBContainer className="box">
           <h1>Account Settings</h1>
-          <MDBContainer className="form-group">
-            <label htmlFor="firstnameText">First Name:</label>
-            <input placeholder="SIS" className="form-control textBox" id="firstnameText"/>
-            <label htmlFor="lastnameText">Last Name:</label>
-            <input placeholder="Man" className="form-control textBox" id="lastnameText"/>
-            <label htmlFor="usernametext">Username:</label>
-            <input placeholder="mans" className="form-control textBox" id="usernametext" readOnly/>
-            <label htmlFor="emailText">Email:</label>
-            <input placeholder="sisman@rpi.edu" className="form-control textBox" id="emailText"/>
+          <MDBContainer>
+            <MDBRow className="accountInputs">
+              <MDBCol md="6">
+                <label htmlFor="firstnameText">First Name:</label>
+                <input placeholder="SIS" className="form-control textBox" id="firstnameText"/>
+              </MDBCol>
+              <MDBCol md="6">
+                <label htmlFor="lastnameText">Last Name:</label>
+                <input placeholder="Man" className="form-control textBox" id="lastnameText"/>
+              </MDBCol>
+            </MDBRow>
+
+            <MDBRow className="accountInputs">
+                <MDBCol md="6">
+                  <label htmlFor="usernametext">Username:</label>
+                  <input placeholder="mans" className="form-control textBox" id="usernametext" readOnly disabled/>
+                </MDBCol>
+                <MDBCol md="6">
+                  <label htmlFor="emailText">Email:</label>
+                  <input placeholder="sisman@rpi.edu" className="form-control textBox" id="emailText"/>
+                </MDBCol>
+            </MDBRow>
+
+            <MDBRow className="accountInputs">
+            <MDBCol md="6">
+                <label htmlFor="institution">Institution:</label>
+                <input placeholder="Rensselaer Polytechnic Institute" className="form-control textBox" id="institution"/>
+              </MDBCol>
+              <MDBCol md="6">
+                <label htmlFor="passwordChange">Password:</label>
+                <a id="passwordChange">Click here to change</a>
+              </MDBCol>              
+            </MDBRow>
 
             { /* TODO: Hide these until the user clicks the change password button below */ }
-            <label htmlFor="newPasswordText">New password:</label>
-            <input type="password" placeholder="••••••••••••" className="form-control textBox" id="newPasswordText"/>
-            <label htmlFor="confirmNewPassword">Confirm new password:</label>
-            <input type="password" placeholder="••••••••••••" className="form-control textBox" id="confirmNewPassword"/>
-            <label htmlFor="currentPasswordText">Verify changes with current password:</label>
-            <input type="password" placeholder="••••••••••••" className="form-control textBox" id="currentPasswordText"/>
+            <MDBContainer style={{display:"none"}}>
+              <label htmlFor="newPasswordText">New password:</label>
+              <input type="password" placeholder="••••••••••••" className="form-control textBox" id="newPasswordText"/>
+              <label htmlFor="confirmNewPassword">Confirm new password:</label>
+              <input type="password" placeholder="••••••••••••" className="form-control textBox" id="confirmNewPassword"/>
+              <label htmlFor="currentPasswordText">Verify changes with current password:</label>
+              <input type="password" placeholder="••••••••••••" className="form-control textBox" id="currentPasswordText"/>
+            </MDBContainer>
           </MDBContainer>
 
-          <Link to={"/login/forgot"}>
-            <button className="button">Change password</button>
+          <Link id="saveChanges" to={"/login/forgot"}>
+            <button className="button">Save Changes</button>
           </Link>
         </MDBContainer>
       </MDBContainer>
