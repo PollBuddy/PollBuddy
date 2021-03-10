f54581b97e8bbfe3d1639c65f072da7f72326240
Isaac-the-Man <steven97102@gmail.com>
2021-03-10 08:37:40 -0500

    JS Doc for createResponse

--- PollBuddy-Server/backend/modules/utils.js
+++ PollBuddy-Server/backend/modules/utils.js
@@ -1,8 +1,16 @@
 const mongoConnection = require("../modules/mongoConnection.js");
 
 
-// Helper function for creating specified http response (https://pollbuddy.app/api/users)
-// both data and error are optional
+/**
+ * Helper function for creating the specified http response (https://pollbuddy.app/api/users). For sample usage see (https://github.com/PollBuddy/PollBuddy/wiki/Specifications-%E2%80%90-Backend-Overview#helper-functions
+ * @param {*} [data] - data payload returned to the callee upon a successful call
+ * @param {string} [error] - error message returned to the callee upon a failed call
+ * @typedef {Object} HttpResponse
+ * @property {string} result - "success" or "failure", depending on if param "error" is present
+ * @property {*} [data] - "data" passed in the param
+ * @property {string} [error] - "error" passed in the param
+ * @returns {HttpResponse}
+ */
 function createResponse(data, error) {
   const payload = {
     result: !error ? "success" : "failure"
