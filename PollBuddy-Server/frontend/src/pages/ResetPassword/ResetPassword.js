b363dc63a0e35a610bdbad5dce2fcf0adc84c43f
pathfold <52586263+pathfold@users.noreply.github.com>
2021-07-20 15:15:52 -0400

    added toggle option to log out everywhere, additional bug fixes for form buttons

--- PollBuddy-Server/frontend/src/pages/ResetPassword/ResetPassword.js
+++ PollBuddy-Server/frontend/src/pages/ResetPassword/ResetPassword.js
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
 import {MDBContainer} from "mdbreact";
 import {Link} from "react-router-dom";
+import './ResetPassword.scss';
 
 import "mdbreact/dist/css/mdb.css";
 
 export default class ResetPassword extends Component {
+  constructor() {
+    super();
+    this.state = {logOutCheck: true}
+    this.handleLogOutCheck = this.handleLogOutCheck.bind(this);
+  }
+
+  handleLogOutCheck() {
+    this.setState({logOutCheck: !this.state.logOutCheck});
+  }
 
   componentDidMount() {
     this.props.updateTitle("Reset Password");
@@ -24,6 +34,10 @@ export default class ResetPassword extends Component {
             <input type="password" placeholder="••••••••••••" className="form-control textBox" id="newPasswordText"/>
             <label htmlFor="confirmPasswordText">Confirm password:</label>
             <input type="password" placeholder="••••••••••••" className="form-control textBox" id="confirmPasswordText"/>
+            <div id="logOutEverywhereContainer">
+              <input type="checkbox" onChange={this.handleLogOutCheck} className="logOutBox" id="logOutEverywhere" checked={this.logOutCheck}/>
+              <label className="logOutLabel" for="logOutEverywhere">Log out everywhere</label>
+            </div>
           </MDBContainer>
 
           <Link to={"/Groups"}>
