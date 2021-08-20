8154d1419cd822b50fbf1084fb4aa878495e7279
pathfold <52586263+pathfold@users.noreply.github.com>
2021-08-20 18:41:33 -0400

    formmating issues cont

--- PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
+++ PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
@@ -4,21 +4,21 @@ import "./Popup2.scss";
 
 export default class Popup2 extends React.Component {
    
-    render() {
-        return (
-            <div class="shell">
-                {this.props.dim && <div class="modal-overlay"/>}
-                <div class={this.props.dim ? "modal_dim" : "modal_nodim"}>
-                    <div class="modal-topbar">
-                    </div>
-                    <div class="modal-container">
-                        <div class="modal-guts">
-                            <p class="modal-text">{this.props.text}</p>
-                            <button class="close-button" onClick={this.props.handleModal}>CLOSE</button>
-                        </div>
+  render() {
+    return (
+        <div class="shell">
+            {this.props.dim && <div class="modal-overlay"/>}
+            <div class={this.props.dim ? "modal_dim" : "modal_nodim"}>
+                <div class="modal-topbar">
+                </div>
+                <div class="modal-container">
+                    <div class="modal-guts">
+                        <p class="modal-text">{this.props.text}</p>
+                        <button class="close-button" onClick={this.props.handleModal}>CLOSE</button>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
 }
