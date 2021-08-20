b4d920c85c42ad56e588e14bd9a74a29d96f45fc
pathfold <52586263+pathfold@users.noreply.github.com>
2021-08-20 18:45:29 -0400

    formatting cont3

--- PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
+++ PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
@@ -6,19 +6,19 @@ export default class Popup2 extends React.Component {
    
   render() {
     return (
-        <div class="shell">
-            {this.props.dim && <div class="modal-overlay"/>}
-            <div class={this.props.dim ? "modal_dim" : "modal_nodim"}>
-                <div class="modal-topbar">
-                </div>
-                <div class="modal-container">
-                    <div class="modal-guts">
-                        <p class="modal-text">{this.props.text}</p>
-                        <button class="close-button" onClick={this.props.handleModal}>CLOSE</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+			<div class="shell">
+					{this.props.dim && <div class="modal-overlay"/>}
+					<div class={this.props.dim ? "modal_dim" : "modal_nodim"}>
+							<div class="modal-topbar">
+							</div>
+							<div class="modal-container">
+									<div class="modal-guts">
+											<p class="modal-text">{this.props.text}</p>
+											<button class="close-button" onClick={this.props.handleModal}>CLOSE</button>
+									</div>
+							</div>
+					</div>
+			</div>
     )
 }
 }
