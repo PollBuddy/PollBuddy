555b3de41c8aaa94ed394535f2d010c9845fc572
hbh7 <hbh7@hbh7.com>
2021-08-20 21:10:26 -0400

    formatting cont4

--- PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
+++ PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
@@ -3,22 +3,22 @@ import React from "react";
 import "./Popup2.scss";
 
 export default class Popup2 extends React.Component {
-   
+
   render() {
     return (
-			<div class="shell">
-					{this.props.dim && <div class="modal-overlay"/>}
-					<div class={this.props.dim ? "modal_dim" : "modal_nodim"}>
-							<div class="modal-topbar">
-							</div>
-							<div class="modal-container">
-									<div class="modal-guts">
-											<p class="modal-text">{this.props.text}</p>
-											<button class="close-button" onClick={this.props.handleModal}>CLOSE</button>
-									</div>
-							</div>
-					</div>
-			</div>
-    )
-}
+      <div className="shell">
+        {this.props.dim && <div className="modal-overlay"/>}
+        <div className={this.props.dim ? "modal_dim" : "modal_nodim"}>
+          <div className="modal-topbar">
+          </div>
+          <div className="modal-container">
+            <div className="modal-guts">
+              <p className="modal-text">{this.props.text}</p>
+              <button className="close-button" onClick={this.props.handleModal}>CLOSE</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
