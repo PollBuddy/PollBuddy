ab756655f6c7bde8afea15bb33dc4861ba8c950a
hbh7 <hbh7@hbh7.com>
2021-10-05 17:34:36 -0400

    Fixed some CSS style scoping

--- PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
+++ PollBuddy-Server/frontend/src/components/Popup2/Popup2.js
@@ -7,14 +7,14 @@ export default class Popup2 extends React.Component {
   render() {
     return (
       <div className="shell">
-        {this.props.dim && <div className="modal-overlay"/>}
-        <div className={this.props.dim ? "modal_dim" : "modal_nodim"}>
-          <div className="modal-topbar">
+        {this.props.dim && <div className="popup2-modal-overlay"/>}
+        <div className={this.props.dim ? "popup2-modal_dim" : "popup2-modal_nodim"}>
+          <div className="popup2-modal-topbar">
           </div>
-          <div className="modal-container">
-            <div className="modal-guts">
-              <p className="modal-text">{this.props.text}</p>
-              <button className="close-button" onClick={this.props.handleModal}>CLOSE</button>
+          <div className="popup2-modal-container">
+            <div className="popup2-modal-guts">
+              <p className="popup2-modal-text">{this.props.text}</p>
+              <button className="popup2-close-button" onClick={this.props.handleModal}>CLOSE</button>
             </div>
           </div>
         </div>
