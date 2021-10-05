b3342d7ab0a09ff3db85371afb5744596595ac28
simar-singh <simar.singh.r@gmail.com>
2021-10-05 16:30:55 -0400

    Fixed Linting issues
    Fixed the linting issues on the validator

--- PollBuddy-Server/frontend/src/utils/validator.js
+++ PollBuddy-Server/frontend/src/utils/validator.js
@@ -1,28 +1,28 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 /* Define Schema as per Wiki requirements for each field */
 const schema = Joi.object({
   username: Joi.string()
-    .pattern(new RegExp('^(?=.{3,32}$)[a-zA-Z0-9\-._]+$'))
-    .error(new Error('Username must be between 3 and 32 characters. Valid characters include letters, numbers, underscores, dashes, and periods.')),
+    .pattern(new RegExp("^(?=.{3,32}$)[a-zA-Z0-9\-._]+$"))
+    .error(new Error("Username must be between 3 and 32 characters. Valid characters include letters, numbers, underscores, dashes, and periods.")),
   email: Joi.string().email({ tlds: {allow: false}, minDomainSegments: 2})
     .max(320)
-    .error(new Error('Invalid email format.')),
+    .error(new Error("Invalid email format.")),
   password: Joi.string()
-    .pattern(new RegExp('^(?=.{10,256})(?:(.)(?!\\1\\1\\1))*$'))
-    .pattern(new RegExp('^.*[0-9].*$'))
-    .pattern(new RegExp('^.*[A-Z].*$'))
-    .error(new Error('Invalid password. Must contain 10 or more characters, ' +
-      'at least 1 uppercase letter, and at least 1 number. ' +
-      'Cannot have 4 of the same characters in a row.')),
+    .pattern(new RegExp("^(?=.{10,256})(?:(.)(?!\\1\\1\\1))*$"))
+    .pattern(new RegExp("^.*[0-9].*$"))
+    .pattern(new RegExp("^.*[A-Z].*$"))
+    .error(new Error("Invalid password. Must contain 10 or more characters, " +
+      "at least 1 uppercase letter, and at least 1 number. " +
+      "Cannot have 4 of the same characters in a row.")),
   firstname: Joi.string()
     .min(1)
     .max(256)
-    .error(new Error('First name must be between 1 and 256 characters.')),
+    .error(new Error("First name must be between 1 and 256 characters.")),
   lastname: Joi.string()
-    .allow('')
+    .allow("")
     .max(256)
-    .error(new Error('Last name must be less than 256 characters.')),
+    .error(new Error("Last name must be less than 256 characters.")),
 });
 
 /**
@@ -34,18 +34,18 @@ const schema = Joi.object({
 function validateFieldJOI(field, val) {
   // Validate value with corresponding field
   switch(field.toString()) {
-    case 'firstName':
-      return schema.validate({ firstname: val });
-    case 'lastName':
-      return schema.validate({ lastname: val });
-    case 'userName':
-      return schema.validate({ username: val });
-    case 'password':
-      return schema.validate({ password: val });
-    case 'email':
-      return schema.validate({ email: val });
-    default:
-      break;
+  case "firstName":
+    return schema.validate({ firstname: val });
+  case "lastName":
+    return schema.validate({ lastname: val });
+  case "userName":
+    return schema.validate({ username: val });
+  case "password":
+    return schema.validate({ password: val });
+  case "email":
+    return schema.validate({ email: val });
+  default:
+    break;
   }
 }
 
@@ -60,7 +60,7 @@ export default function validateUserData(userData) {
   for(let field in userData) {
     let valid = validateFieldJOI(field, userData[field]);
 
-    userObj[field] = { valid: !valid.error ? true : false, value: valid ? userData[field] : null, error: valid.error ? valid.error.toString() : null  }
+    userObj[field] = { valid: !valid.error ? true : false, value: valid ? userData[field] : null, error: valid.error ? valid.error.toString() : null  };
   }
 
   return userObj;
