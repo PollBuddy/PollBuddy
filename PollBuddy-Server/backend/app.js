162f67628993e8d08f702c78621416467d8df6c5
hbh7 <hbh7@hbh7.com>
2021-10-12 17:31:10 -0400

    Small formatting tweaks

--- PollBuddy-Server/backend/app.js
+++ PollBuddy-Server/backend/app.js
@@ -195,9 +195,9 @@ app.get("/api", function (req, res, next) {
 });
 
 /**
- * fall-through response for undefined routes
- * kicks a 404 error down to the next middleware(the error handler)
- * For full documnetation see the wiki https://github.com/PollBuddy/PollBuddy/wiki/Specifications-%E2%80%90-Backend-Routes-(Overview)
+ * Fall-through response for undefined routes
+ * Sends a 404 error down to the next middleware (the error handler)
+ * For full documentation, see the wiki: https://github.com/PollBuddy/PollBuddy/wiki/Specifications-%E2%80%90-Backend-Routes-(Overview)
  * @throws 404 - Not Found
  * @param {function} callback - Function handler for endpoint.
  */
@@ -207,15 +207,14 @@ app.use(function (req, res, next) {
 
 
 /**
- * final error handler for errors in the backend
- * errors in the backend are sent to this function for handling
- * the error is logged in the console
- * as well as the error message
- * if this is the development environment, the entire error is placed in the response as well under res.locals.err
- * if the error has an associated status it is sent in the response
- * if not simply send 500 as the status (generic server error)
- * For full documnetation see the wiki https://github.com/PollBuddy/PollBuddy/wiki/Specifications-%E2%80%90-Backend-Routes-(Overview)
- * @throws error code of given error or 500 - server error
+ * Final error handler for errors in the backend.
+ * Errors in the backend are sent to this function for handling.
+ * The error is logged in the console, as well as the error message.
+ * If this is the development environment, the entire error is placed in the response as well under res.locals.err.
+ * If the error has an associated status, it is sent in the response.
+ * If not, it sends 500 as the status (generic server error)
+ * For full documentation, see the wiki: https://github.com/PollBuddy/PollBuddy/wiki/Specifications-%E2%80%90-Backend-Routes-(Overview)
+ * @throws error code of given error, or 500 - server error
  * @param {function} callback - Function handler for endpoint.
  */
 app.use(function (err, req, res, next) {
