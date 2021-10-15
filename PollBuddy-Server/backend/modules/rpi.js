6ff533cabfd011186e701c762148ec9304691882
Ashwin Chitoor <ashketchup55@gmail.com>
2021-10-15 16:16:02 -0400

    Update CAS URL

--- PollBuddy-Server/backend/modules/rpi.js
+++ PollBuddy-Server/backend/modules/rpi.js
@@ -6,7 +6,7 @@ schoolList.append("Rensselaer Polytechnic Institute (RPI)", "rpi");
 
 const cas = new CASAuthentication({
   /* eslint-disable camelcase */
-  cas_url: "https://cas-auth.rpi.edu/cas",
+  cas_url: "https://cas.auth.rpi.edu/cas",
   service_url: process.env["FRONTEND_URL"],
   cas_version: "3.0",
   /* eslint-enable camelcase */
