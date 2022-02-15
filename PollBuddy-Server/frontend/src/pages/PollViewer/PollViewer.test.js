13fd931890705a683ac935e1bde70555ef61756d
PrinxeShamar <44654743+PrinxeShamar@users.noreply.github.com>
2022-02-15 16:22:47 -0500

    Final Updates

--- PollBuddy-Server/frontend/src/pages/PollViewer/PollViewer.test.js
+++ PollBuddy-Server/frontend/src/pages/PollViewer/PollViewer.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PollViewer from "./PollViewer";
+import {BrowserRouter} from "react-router-dom";
 
 function updateTitle() {
   return false;
@@ -11,7 +12,10 @@ it("renders without crashing", () => {
   // Create div element
   const div = document.createElement("div");
   // Render about on the div
-  ReactDOM.render(<PollViewer updateTitle={updateTitle}/>, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <PollViewer updateTitle={updateTitle}/>
+    </BrowserRouter>, div);
   // Clean unmount
   ReactDOM.unmountComponentAtNode(div);
 });
