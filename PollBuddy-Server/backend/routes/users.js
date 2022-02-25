a80bb9395f03d725ad4fbd907b6cdc2108891050
hbh7 <hbh7@hbh7.com>
2022-02-25 17:50:16 -0500

    Fix for registering with RPI

--- PollBuddy-Server/backend/routes/users.js
+++ PollBuddy-Server/backend/routes/users.js
@@ -432,8 +432,8 @@ router.get("/register/rpi", rpi.bounce, function (req, res) {
     delete req.session.cas_user;
 
     // Send the user to the registration step 2 page with relevant info to prefill
-    return res.redirect("/register/school/step2?result=success&userName=" + req.session.userDataTemp.userName +
-      "&email=" + req.session.userDataTemp.email + "&school=rpi");
+    return res.redirect("/register/school/step2?result=success&data=" + JSON.stringify({ "userName":
+      req.session.userDataTemp.userName, "email": req.session.userDataTemp.email, "school": "rpi"}));
 
   } else {
     // Something went wrong
