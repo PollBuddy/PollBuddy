401d36a9beb4c69f6fc135423cd193570053aea2
Khirupa <khiruparaj@gmail.com>
2022-03-06 22:28:44 +0530

    fix(backend): modified JSON response format for 404 error.

--- PollBuddy-Server/backend/app.js
+++ PollBuddy-Server/backend/app.js
@@ -139,7 +139,7 @@ app.get("/api", function (req, res) {
  * @param {function} callback - Function handler for endpoint.
  */
 app.use(function (req, res, next) {
-  res.status(404).send(createError("Page not found"));
+  res.status(404).send(createResponse(null, "API route not found."));
   next();
 });
 
