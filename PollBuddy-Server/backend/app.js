f4fc72c3e5d8aa5184f430960200701e3490679b
hbh7 <hbh7@hbh7.com>
2022-03-18 17:23:35 -0400

    Turns out next() definitely needs to not be there

--- PollBuddy-Server/backend/app.js
+++ PollBuddy-Server/backend/app.js
@@ -144,7 +144,6 @@ app.get("/api", function (req, res) {
 // eslint-disable-next-line no-unused-vars
 app.use(function (req, res, next) {
   res.status(404).send(createResponse(null, "API route not found."));
-  next();
 });
 
 
