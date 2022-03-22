246c72e3ffdb38541555da0af1bde0f405f47c73
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2022-03-22 17:33:22 -0400

    Comments

--- PollBuddy-Server/frontend/src/components/PrivateComponent/PrivateComponent.js
+++ PollBuddy-Server/frontend/src/components/PrivateComponent/PrivateComponent.js
@@ -2,6 +2,10 @@ import React from "react";
 import {Navigate} from "react-router-dom";
 import { withRouter } from "../PropsWrapper/PropsWrapper";
 
+// Private component is used to redirect users based on if the are logged in or not.
+// Pass in state = {true} if the user needs to be logged in to access a route.
+// Pass in state = {false} if the user needs to be logged out to access a route.
+// If it doesn't matter whether the user is logged in or not, don't use PrivateComponent.
 class PrivateComponent extends React.Component {
   constructor(props) {
     super(props);
