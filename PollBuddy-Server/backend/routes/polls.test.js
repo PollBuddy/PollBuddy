4d2a9ac1f0cd4f846dde23039e4342de64bf185e
Kevin Xu <35581753+dubiousCat@users.noreply.github.com>
2022-04-04 17:37:11 -0400

    lint test fixes 3

--- PollBuddy-Server/backend/routes/polls.test.js
+++ PollBuddy-Server/backend/routes/polls.test.js
@@ -8,8 +8,8 @@ const MongoClient = mongo.MongoClient;
 var mongoConnection = require("../modules/mongoConnection.js");
 var pollsRouter = require("./polls");
 
-var { testUser, testUser2, testGroup, samplequestion2, createUser, createGroup, createPollWithQuestion } = require("../modules/testingUtils.js");
-const {createPoll, testPoll, testPoll2} = require("../modules/testingUtils");
+var { testUser, testUser2, testGroup, createUser, createGroup, createPollWithQuestion } = require("../modules/testingUtils.js");
+const {createPoll, testPoll, testPoll2, samplequestion2} = require("../modules/testingUtils");
 const bson = require("bson");
 
 let mockApp = express();
@@ -456,23 +456,23 @@ describe("/api/polls/:pollID/editQuestion", () => {
     session = { userData: { userID: user.insertedId } };
 
     await app.post("/api/polls/" + poll.insertedId + "/editQuestion")
-      .send({
-        id: poll.insertedId.Questions[0]._id,
-        text: "sample.question2",
-        answers: [{ text: "sample.answer", correct: true }, { text: "sample.answer2", correct: false }],
-        maxAllowedChoices: 1,
-      })
-      .expect(200)
-      .then(async (response) => {
-        expect(response.body.result).toBe("success");
-        let questionRes = await mongoConnection.getDB().collection("polls").findOne({
-          "_id": poll.insertedId,
-          "Questions._id": poll.insertedId.Questions[0]._id,
+        .send({
+          id: poll.insertedId.Questions[0]._id,
+          text: "sample.question2",
+          answers: [{ text: "sample.answer", correct: true }, { text: "sample.answer2", correct: false }],
+          maxAllowedChoices: 1,
+        })
+        .expect(200)
+        .then(async (response) => {
+          expect(response.body.result).toBe("success");
+          let questionRes = await mongoConnection.getDB().collection("polls").findOne({
+            "_id": poll.insertedId,
+            "Questions._id": poll.insertedId.Questions[0]._id,
+          });
+          expect(questionRes.text).toEqual(samplequestion2.text);
+          expect(questionRes.answers).toEqual(samplequestion2.answers);
+          expect(questionRes.maxAllowedChoices).toEqual(samplequestion2.maxAllowedChoices);
         });
-        expect(questionRes.text).toEqual(samplequestion2.text);
-        expect(questionRes.answers).toEqual(samplequestion2.answers);
-        expect(questionRes.maxAllowedChoices).toEqual(samplequestion2.maxAllowedChoices);
-      });
   });
 
   it("POST: edit question as non-admin/pollcreator, failure", async () => {
