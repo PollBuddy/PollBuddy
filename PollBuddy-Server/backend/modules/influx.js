f202cbc6566a50b5d239e55b42bbb051ab4dd9de
hbh7 <hbh7@hbh7.com>
2022-04-16 15:47:24 -0400

    Reporting DB connection attempt 1

--- PollBuddy-Server/backend/modules/influx.js
+++ PollBuddy-Server/backend/modules/influx.js
@@ -1,10 +1,23 @@
 const Influx = require("influx");
-let influx = new Influx.InfluxDB({
-  host: "reporting-db", // You must run in Docker-Compose for this to work
-  database: "pollbuddy",
-  username: "pollbuddy",
-  password: "pollbuddy"
-});
+let influx;
+
+if(process.env.REPORTING_DB_URL) {
+  influx = new Influx.InfluxDB({
+    // You must run in Kubernetes for this to work by default
+    host: process.env.REPORTING_DB_URL,
+    database: process.env.REPORTING_DB_NAME,
+    username: process.env.REPORTING_DB_USERNAME,
+    password: process.env.REPORTING_DB_PASSWORD
+  });
+} else {
+  influx = new Influx.InfluxDB({
+    // You must run in Docker-Compose for this to work by default
+    host: "reporting-db",
+    database: "pollbuddy",
+    username: "pollbuddy",
+    password: "pollbuddy"
+  });
+}
 
 module.exports = {
 
