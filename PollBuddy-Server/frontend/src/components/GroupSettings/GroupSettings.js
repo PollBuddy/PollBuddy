147dec57bd7cc45d6d415c2ff09cb12a03186aeb
Curtis Terry <curtisymo90@gmail.com>
2022-07-19 14:39:47 -0400

    started addng invite member

--- PollBuddy-Server/frontend/src/components/GroupSettings/GroupSettings.js
+++ PollBuddy-Server/frontend/src/components/GroupSettings/GroupSettings.js
@@ -38,6 +38,12 @@ class GroupSettings extends Component{
     this.props.router.navigate("/groups");
   };
 
+  getGroupCode = async () => {
+    var code;
+    code = this.state.id;
+    return code;
+  }
+
   render(){
     if (this.state.isMember) {
       return (
@@ -70,6 +76,13 @@ class GroupSettings extends Component{
             onClick={this.handleDeleteGroup}
           >Delete this Group
           </button>
+
+          <button style={{width: "17em"}}
+            className="button"
+            onClick={display.this.getGroupCode}
+          >Get Invite Code
+          </button>
+          
         </MDBContainer>
       );
     }
