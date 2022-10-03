2ea186d4c1ce88e516db486c1c2a1f5e4ac70a87
MajorDroolz <maximhutz@gmail.com>
2022-10-02 20:59:08 -0400

    CLEAN: Header now functional component.

--- PollBuddy-Server/frontend/src/components/Header/Header.js
+++ PollBuddy-Server/frontend/src/components/Header/Header.js
@@ -4,36 +4,36 @@ import "./Header.scss";
 import logo from "../../images/logo.png";
 import Dropdown from "../Dropdown/Dropdown.js";
 
-export default class Header extends Component {
-  constructor(props) {
-    super(props);
-    // TODO: This needs further reworking. I'm thinking it always shows login unless it detects a certain page title,
-    // in which case it may display register. This also will need to show account once logged in.
-    if(this.props.userInfo.sessionIdentifier === "") {
-      this.state = {
-        link: "/login",
-        text: "login"
-      };
-    } else if(this.props.userInfo.sessionIdentifier === "register") {
-      this.state = {
-        link: "/register",
-        text: "register"
-      };
-    } else if(this.props.userInfo.sessionIdentifier === "account") {
-      this.state = {
-        link: "/AccountInfo",
-        text: "account"
-      };
-    }
-  }
-  render() {
-    return (
-      <header className = "Header-bar">
-        <a href = "/">
-          <img src = {logo} className = "Header-bar-logo" alt = "logo" />
-        </a>
-        <Dropdown />
-      </header>
-    );
-  }
+/*----------------------------------------------------------------------------*/
+
+function Header({ userInfo: { sessionIdentifier: sessionID } }) {
+  // let link, text;
+
+  // switch (sessionID) {
+  // case "":
+  //   link = "/login";
+  //   text = "login";
+  //   break;
+  // case "register":
+  //   link = "/register";
+  //   text = "register";
+  //   break;
+  // case "account":
+  //   link = "/AccountInfo";
+  //   text = "account";
+  //   break;
+  // }
+
+  return (
+    <header className="Header-bar">
+      <a href="/">
+        <img src = {logo} className="Header-bar-logo" alt="logo" />
+      </a>
+      <Dropdown />
+    </header>
+  );
 }
+
+/*----------------------------------------------------------------------------*/
+
+export default Header;
