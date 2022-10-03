0f820bf3be459ad99d7f56f424553167747a51f1
MajorDroolz <maximhutz@gmail.com>
2022-10-02 20:08:56 -0400

    CLEAN: Turned Footer.js to functional component.

--- PollBuddy-Server/frontend/src/components/Footer/Footer.js
+++ PollBuddy-Server/frontend/src/components/Footer/Footer.js
@@ -3,41 +3,45 @@ import "mdbreact/dist/css/mdb.css";
 import "./Footer.scss";
 import rcosLogo from "../../images/rcos.png";
 import githubLogo from "../../images/github.png";
-import {MDBContainer} from "mdbreact";
+import { MDBContainer } from "mdbreact";
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer className = "Footer-foot">
-        <MDBContainer className = "Footer-foot-links">
-          <span><a href = "/about">
+export default function Footer({ }) {
+  return (
+    <footer className="Footer-foot">
+      <MDBContainer className="Footer-foot-links">
+        <span>
+          <a href="/about">
             About
           </a>
-          <a href = "https://info.rpi.edu/statement-of-accessibility" target = "_blank" rel = "noopener noreferrer">
+          <a href="https://info.rpi.edu/statement-of-accessibility"
+             target="_blank" rel="noopener noreferrer">
             Accessibility
-          </a></span>
-          <span><a href = "/contact">
+          </a>
+        </span>
+        <span>
+          <a href="/contact">
             Contact
           </a>
-          <a href = "/faq">
+          <a href="/faq">
             FAQ
           </a>
-          <a href = "/privacy">
+          <a href="/privacy">
             Privacy
-          </a></span>
-        </MDBContainer>
-        <MDBContainer className = "Footer-foot-links Footer-copy">
-          <span>&copy; 2019-{new Date().getFullYear()} Poll Buddy</span>
-        </MDBContainer>
-        <MDBContainer className = "Footer-foot-links">
-          <a href = "https://rcos.io/" target = "_blank" rel="noopener noreferrer">
-            <img src = {rcosLogo} alt = "RCOS" />
           </a>
-          <a href = "https://github.com/PollBuddy/PollBuddy" target = "_blank" rel="noopener noreferrer">
-            <img src = {githubLogo} alt = "Github" />
-          </a>
-        </MDBContainer>
-      </footer>
-    );
-  }
+        </span>
+      </MDBContainer>
+      <MDBContainer className="Footer-foot-links Footer-copy">
+        <span>&copy; 2019-{new Date().getFullYear()} Poll Buddy</span>
+      </MDBContainer>
+      <MDBContainer className="Footer-foot-links">
+        <a href="https://rcos.io/" target="_blank" rel="noopener noreferrer">
+          <img src={rcosLogo} alt="RCOS" />
+        </a>
+        <a href="https://github.com/PollBuddy/PollBuddy" target="_blank"
+           rel="noopener noreferrer">
+          <img src={githubLogo} alt="Github" />
+        </a>
+      </MDBContainer>
+    </footer>
+  );
 }
