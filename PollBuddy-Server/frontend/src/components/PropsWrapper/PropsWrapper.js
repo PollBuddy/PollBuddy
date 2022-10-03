cb3488a27f12e6d96cfdaeb42815f484f3220432
MajorDroolz <maximhutz@gmail.com>
2022-10-02 21:27:18 -0400

    CLEAN: Cleaned up withRouter HoC.

--- PollBuddy-Server/frontend/src/components/PropsWrapper/PropsWrapper.js
+++ PollBuddy-Server/frontend/src/components/PropsWrapper/PropsWrapper.js
@@ -1,15 +1,27 @@
 import React from "react";
-import {useParams, useLocation, useNavigate, useSearchParams} from "react-router-dom";
+import {
+  useParams, useLocation, useNavigate, useSearchParams
+} from "react-router-dom";
 
-// Apparently this is necessary in React-Router V6 to get access to the URL parameters with a class-based component...
-export function withRouter(C) {
+/*----------------------------------------------------------------------------*/
+
+// Apparently this is necessary in React-Router V6 to get access to the URL
+// parameters with a class-based component...
+function withRouter(C) {
   function ComponentWithRouterProp(props) {
-    let location = useLocation();
-    let navigate = useNavigate();
-    let params = useParams();
-    const [searchParams, setSearchParams] = useSearchParams();
-    return (<C {...props} router={{ location, navigate, params, searchParams }} />);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const params = useParams();
+    const [ searchParams, ] = useSearchParams();
+    
+    return (
+      <C {...props} router={{ location, navigate, params, searchParams }} />
+    );
   }
 
   return ComponentWithRouterProp;
 }
+
+/*----------------------------------------------------------------------------*/
+
+export { withRouter };
