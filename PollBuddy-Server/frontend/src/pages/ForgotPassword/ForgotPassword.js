fcec60634e11bd95a8c67a3f9942ff433072aa2a
MajorDroolz <maximhutz@gmail.com>
2022-10-03 23:47:59 -0400

    CLEAN: ForgotPassword page now functional component.

--- PollBuddy-Server/frontend/src/pages/ForgotPassword/ForgotPassword.js
+++ PollBuddy-Server/frontend/src/pages/ForgotPassword/ForgotPassword.js
@@ -1,60 +1,62 @@
-import React, {Component} from "react";
-import {MDBContainer} from "mdbreact";
+import React from "react";
+import { MDBContainer } from "mdbreact";
 import "mdbreact/dist/css/mdb.css";
 import { withRouter } from "../../components/PropsWrapper/PropsWrapper";
 
-class ForgotPassword extends Component {
+/*----------------------------------------------------------------------------*/
 
-  constructor(props){
-    super(props);
-    this.state = {
-      emailInput : "",
-    };
-    this.requestReset = this.requestReset.bind(this);
-  }
+function ForgotPassword({ updateTitle, router }) {
+  const [ emailInput, setEmailInput ] = React.useState("");
 
-  componentDidMount() {
-    this.props.updateTitle("Forgot Password");
-  }
+  React.useEffect(() => {
+    updateTitle?.("Forgot Password");
+  }, [ updateTitle ]);
 
-  requestReset(){
-    fetch(process.env.REACT_APP_BACKEND_URL + "/users/forgotpassword/submit", {
+  const requestReset = React.useCallback(async () => {
+    const URL = process.env.REACT_APP_BACKEND_URL +
+                "/users/forgotpassword/submit";
+            
+    const response = await fetch(URL, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },//HEADERS LIKE SO ARE NECESSARY for some reason https://stackoverflow.com/questions/39842013/fetch-post-with-body-data-not-working-params-empty
-      body: JSON.stringify({"email" : this.state.emailInput})
-    }).then(response => response.json())
-      .then(
-        val => {
-          if(!(val.result === "success")){
-            console.log("failed to update data");
-          }else{
-            const { router } = this.props;
-            router.navigate("/login/reset");
-          }
-        },
-        err => {console.log(err);}
-      );
-  }
+      // HEADERS LIKE SO ARE NECESSARY for some reason
+      // https://stackoverflow.com/questions/39842013/fetch-post-with-body-data-not-working-params-empty
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ "email" : emailInput })
+    });
 
+    try {
+      const val = await response.json();
 
-  render() {
-    return (
-      <MDBContainer fluid className="page">
-        <MDBContainer fluid className="box">
-          <h1>Forgot Your Password?</h1>
-          <p>
-            Enter your email and we will send you a reset.
-          </p>
-          <MDBContainer className="form-group">
-            <label htmlFor="emailText">Email:</label>
-            <input placeholder="Enter email" className="form-control textBox" id="emailText" onChange={(e)=> this.setState({emailInput:e.target.value})}/>
-          </MDBContainer>
-          <button className="button" onClick={this.requestReset}>Reset Password</button>
+      if (val.result !== "success") {
+        console.log("failed to update data");
+      } else {
+        router.navigate("/login/reset");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [ emailInput, router ]);
 
+  return (
+    <MDBContainer fluid className="page">
+      <MDBContainer fluid className="box">
+        <h1>Forgot Your Password?</h1>
+        <p>Enter your email and we will send you a reset.</p>
+        <MDBContainer className="form-group">
+          <label htmlFor="emailText">Email:</label>
+          <input placeholder="Enter email"
+            className="form-control textBox"
+            id="emailText"
+            onChange={e => setEmailInput(e.target.value)}/>
         </MDBContainer>
+        <button className="button" onClick={requestReset}>
+          Reset Password
+        </button>
       </MDBContainer>
-    );
-  }
+    </MDBContainer>
+  );
 }
+
+/*----------------------------------------------------------------------------*/
 
 export default withRouter(ForgotPassword);
