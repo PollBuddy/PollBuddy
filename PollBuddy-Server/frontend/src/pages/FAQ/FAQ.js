40558e41e83bee6a7b29658c22e6a65251ed6de2
MajorDroolz <maximhutz@gmail.com>
2022-10-03 23:06:43 -0400

    CLEAN: FAQ page now functional component.

--- PollBuddy-Server/frontend/src/pages/FAQ/FAQ.js
+++ PollBuddy-Server/frontend/src/pages/FAQ/FAQ.js
@@ -1,35 +1,34 @@
-import React, {Component} from "react";
-import {MDBContainer} from "mdbreact";
+import React from "react";
+import { MDBContainer } from "mdbreact";
 import "mdbreact/dist/css/mdb.css";
 import ReactMarkdown from "react-markdown";
 import faqFile from "./faq.md";
 
-export default class FAQ extends Component {
+/*----------------------------------------------------------------------------*/
 
-  constructor(props) {
-    super(props);
-    this.state = {questions: null};
-  }
+function FAQ({ updateTitle }) {
+  const [ questions, setQuestions ] = React.useState(null);
 
-  componentWillMount() {
-    fetch(faqFile).then((response) => response.text()).then((text) => {
-      this.setState({questions: text});
-    });
-  }
+  React.useEffect(() => {
+    updateTitle?.("Frequestly Asked Questions");
+  }, [ updateTitle ]);
 
-  componentDidMount() {
-    this.props.updateTitle("Frequently Asked Questions");
-  }
+  React.useEffect(async () => {
+    const response = await fetch(faqFile);
+    const text = await response.text();
+    setQuestions(text);
+  }, [ setQuestions ]);
 
-  render() {
-    return (
-      <MDBContainer fluid className="page">
-        <MDBContainer className="box box-body-text">
-          <h1>Frequently Asked Questions</h1>
-          <ReactMarkdown children={this.state.questions}/>
-        </MDBContainer>
+  return (
+    <MDBContainer fluid className="page">
+      <MDBContainer className="box box-body-text">
+        <h1>Frequently Asked Questions</h1>
+        <ReactMarkdown children={questions}/>
       </MDBContainer>
-    );
-  }
-
+    </MDBContainer>
+  );
 }
+
+/*----------------------------------------------------------------------------*/
+
+export default FAQ;
