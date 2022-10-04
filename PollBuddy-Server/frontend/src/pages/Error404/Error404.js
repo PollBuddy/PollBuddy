805782ba7500e168f9ae93bed1c3d1480332cc03
MajorDroolz <maximhutz@gmail.com>
2022-10-03 23:02:46 -0400

    CLEAN: Error404 page now functional component.

--- PollBuddy-Server/frontend/src/pages/Error404/Error404.js
+++ PollBuddy-Server/frontend/src/pages/Error404/Error404.js
@@ -1,22 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import "mdbreact/dist/css/mdb.css";
 import { MDBContainer } from "mdbreact";
 
-export default class Error404 extends Component {
+/*----------------------------------------------------------------------------*/
 
-  componentDidMount(){
-    this.props.updateTitle("Page Not Found");
-  }
-  render() {
-    return (
-      <MDBContainer className="page">
-        <MDBContainer className="box">
-          <p className="fontSizeLarge">
-            Error: page not found.
-          </p>
-        </MDBContainer>
+function Error404({ updateTitle }) {
+  React.useEffect(() => {
+    updateTitle?.("Page Not Found");
+  }, [ updateTitle ]);
+
+  return (
+    <MDBContainer className="page">
+      <MDBContainer className="box">
+        <p className="fontSizeLarge">
+          Error: page not found.
+        </p>
       </MDBContainer>
-
-    );
-  }
+    </MDBContainer>
+  );
 }
+
+/*----------------------------------------------------------------------------*/
+
+export default Error404;
