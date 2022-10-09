c7eeb52fa962a32d4709d8ce2e88f062123a6e4d
MajorDroolz <maximhutz@gmail.com>
2022-10-09 17:33:20 -0400

    CLEAN: Groups page now functional component.

--- PollBuddy-Server/frontend/src/pages/Error404/Error404.test.js
+++ PollBuddy-Server/frontend/src/pages/Error404/Error404.test.js
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Error404 from "./Error404";
 
-function updateTitle() {
-  return false;
-}
-
 // Create basic render test
 it("renders without crashing", () => {
   // Create div element
   const div = document.createElement("div");
   // Render about on the div
-  ReactDOM.render(<Error404 updateTitle={updateTitle}/>, div);
+  ReactDOM.render(<Error404 />, div);
   // Clean unmount
   ReactDOM.unmountComponentAtNode(div);
 });
