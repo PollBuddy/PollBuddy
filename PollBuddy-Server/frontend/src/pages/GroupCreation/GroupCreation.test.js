6092b7acb2357a7a323d329ab6cc6211186fd6ee
nbezanis <nbezanis98@gmail.com>
2022-10-11 16:51:32 -0400

    fix group creation tests

--- PollBuddy-Server/frontend/src/pages/GroupCreation/GroupCreation.test.js
+++ PollBuddy-Server/frontend/src/pages/GroupCreation/GroupCreation.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GroupCreation from "./GroupCreation";
+import {BrowserRouter} from "react-router-dom";
 
 function updateTitle() {
   return false;
@@ -11,7 +12,10 @@ it("renders without crashing", () => {
   // Create div element
   const div = document.createElement("div");
   // Render about on the div
-  ReactDOM.render(<GroupCreation updateTitle={updateTitle}/>, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <GroupCreation updateTitle={updateTitle}/>
+    </BrowserRouter>, div);
   // Clean unmount
   ReactDOM.unmountComponentAtNode(div);
 });
