621f0d45f642115efdcfa18362419ebd72fcf6ea
nbezanis <nbezanis98@gmail.com>
2022-10-18 16:49:36 -0400

    fix contact test

--- PollBuddy-Server/frontend/src/pages/Contact/Contact.test.js
+++ PollBuddy-Server/frontend/src/pages/Contact/Contact.test.js
@@ -6,14 +6,18 @@ function updateTitle() {
   return false;
 }
 
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({text: ""})
+}));
+
 // Create basic render test
 it("renders without crashing", () => {
-  fetch.mockResponseOnce(JSON.stringify({data: {data: {
-    FirstName: "John",
-    LastName: "Doe",
-    SchoolAffiliation: "RPI",
-    email: "johndoe@rpi.edu"
-  }}}));
+  // fetch.mockResponseOnce(JSON.stringify({data: {data: {
+  //   FirstName: "John",
+  //   LastName: "Doe",
+  //   SchoolAffiliation: "RPI",
+  //   email: "johndoe@rpi.edu"
+  // }}}));
   // Create div element
   const div = document.createElement("div");
   // Render about on the div
