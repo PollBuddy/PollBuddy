7b1d78e4e9e8d69e480fab5d0f3bc7f8d7f28c22
MajorDroolz <maximhutz@gmail.com>
2022-10-20 13:33:40 -0400

    FEAT: Unit tests for GroupSettings.

--- PollBuddy-Server/frontend/src/components/GroupSettings/GroupSettings.test.js
+++ PollBuddy-Server/frontend/src/components/GroupSettings/GroupSettings.test.js
@@ -1,38 +1,72 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GroupSettings from "./GroupSettings";
-import {BrowserRouter} from "react-router-dom";
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
-// Create basic render test
-it("renders without crashing", () => {
-  // Create div element
-  const div = document.createElement("div");
-  // Render about on the div
-  ReactDOM.render(
-    <BrowserRouter>
-      <GroupSettings state={{
-        isMember: false,
-        class: "1200 - Data Structures",
-        polls: [
-          {pollId: 1, label: "Big O Notation"},
-          {pollId: 2, label: "Basic C++ Syntax"},
-          {pollId: 3, label: "Pointers"},
-          {pollId: 4, label: "Vectors"},
-          {pollId: 5, label: "Linked Lists"},
-          {pollId: 6, label: "Sets"},
-          {pollId: 7, label: "Maps"}
-        ],
-        total_questions: 24,
-        avg_correct: 20,
-        member_correct: 22,
-        groupData: null,
-        doneLoading: true,
-        id: null,
-        error: null
-        //need to put in groupID from backend
-        //need to get other shit like pollIDs and their respective information...
-      }}/>
-    </BrowserRouter>, div);
-  // Clean unmount
-  ReactDOM.unmountComponentAtNode(div);
+import GroupSettings from './GroupSettings';
+
+const MEM_DATA = {
+  isMember: true,
+  isAdmin: false,
+  id: "TEST",
+}
+
+const ADM_DATA = {
+  isMember: false,
+  isAdmin: true,
+  id: "TEST",
+}
+
+describe("The Footer component:", () => {
+  it("Loads correctly.", () => {
+    // Just make sure it can load.
+    render(<BrowserRouter><GroupSettings state={MEM_DATA}/></BrowserRouter>);
+  });
+
+  it("Leaving group works correctly when Member.", async () => {
+    render(<BrowserRouter><GroupSettings state={MEM_DATA}/></BrowserRouter>);
+    // Can see 'Member Settings'.
+    expect(screen.findByText.bind(this, "Member Settings")).not.toThrow();
+    
+    // Leave work correctly.
+    fetchMock.mockResponseOnce({ status: 200 });
+
+    await act(async () => {
+      await userEvent.click(screen.getByText("Leave Group"))
+    });
+
+    expect(window.location.pathname).toBe("/groups");
+  });
+
+  it("Creating poll works correctly when Admin.", async () => {
+    render(<BrowserRouter><GroupSettings state={ADM_DATA}/></BrowserRouter>);
+    
+    await act(async () => {
+      await userEvent.click(screen.getByText("Create New Poll"));
+    });
+
+    expect(window.location.pathname).toBe("/polls/new");
+  });
+
+  it("Editing poll works correctly when Admin.", async () => {
+    render(<BrowserRouter><GroupSettings state={ADM_DATA}/></BrowserRouter>);
+    
+    await act(async () => {
+      await userEvent.click(screen.getByText("Edit Group"));
+    });
+
+    expect(window.location.pathname).toBe("/groups/TEST/edit");
+  });
+
+  it("Deleting group works correctly when Admin.", async () => {
+    render(<BrowserRouter><GroupSettings state={ADM_DATA}/></BrowserRouter>);
+    fetchMock.mockResponseOnce({ status: 200 });
+
+    await act(async () => {
+      await userEvent.click(screen.getByText("Delete this Group"));
+    });
+
+    expect(window.location.pathname).toBe("/groups");
+  });
 });
