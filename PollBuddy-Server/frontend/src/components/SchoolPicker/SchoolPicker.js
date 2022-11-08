3168df60cb11e1762b5f28b554fce6718d0d280f
MajorDroolz <maximhutz@gmail.com>
2022-11-08 16:41:41 -0500

    FGRGB

--- PollBuddy-Server/frontend/src/components/SchoolPicker/SchoolPicker.js
+++ PollBuddy-Server/frontend/src/components/SchoolPicker/SchoolPicker.js
@@ -1,89 +1,79 @@
-import React, { Component } from "react";
+import React from "react";
 import Autocomplete from "react-autocomplete-pollbuddy";
 import { MDBContainer } from "mdbreact";
 import "mdbreact/dist/css/mdb.css";
 
-export default class SchoolPicker extends Component {
+const BACKEND = process.env.REACT_APP_BACKEND_URL;
 
-  constructor(props) {
-    super(props);
-    this.sortItems = this.sortItems.bind(this);
-    this.renderDropdownItem = this.renderDropdownItem.bind(this);
+function sortItems(itemA, itemB, value) {
+  const lowA = itemA.label.toLowerCase();
+  const lowB = itemB.label.toLowerCase();
 
-    this.state = {
-      "schoolInfo": {
-        "schools": [],
-        "schoolLinkDict": {}
-      }
-    };
+  const indexA = lowA.indexOf(value.toLowerCase());
+  const indexB = lowB.indexOf(value.toLowerCase());
 
-  }
-
-  componentDidMount(){
-    if (this.props.schoolInfo == null) {
-      fetch(process.env.REACT_APP_BACKEND_URL + "/schools", 
-        {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },//HEADERS LIKE SO ARE NECESSARY for some reason https://stackoverflow.com/questions/39842013/fetch-post-with-body-data-not-working-params-empty
-        }    
-      ).then(response => response.json())
-      // handle response
-        .then(data => {
-          let schools = this.state.schoolInfo.schools;
-          let schoolLinkDict = this.state.schoolInfo.schoolLinkDict;
-          for (var i = 0; i < data.length; i++) {
-            schools.push({ key: i, label: data[i][0] });
-            schoolLinkDict[data[i][0]] = data[i][1];
-          }
-          this.setState({"schoolInfo": {"schools": schools, "schoolLinkDict": schoolLinkDict}}); //missing semicolon
-          this.props.onDoneLoading(this.state.schoolInfo);
-        });
-    } else {
-      // Get info that was passed in
-      this.setState({"schoolInfo": this.props.schoolInfo});
-    }
-  }
-
-  sortItems(itemA, itemB, value) {
-    const lowA = itemA.label.toLowerCase();
-    const lowB = itemB.label.toLowerCase();
-    const indexA = lowA.indexOf(value.toLowerCase());
-    const indexB = lowB.indexOf(value.toLowerCase());
-    if (indexA !== indexB) {
-      return (indexA - indexB);
-    }
-    return (lowA < lowB ? -1 : 1);
-  }
-
-  renderDropdownItem(item) {
-    return(
-      <div key={item.key} className="auto_comp">
-        {item.label}
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <MDBContainer className="form-group">
-        <Autocomplete
-          items={this.state.schoolInfo.schools}
-          sortItems={this.sortItems}
-          getItemValue={item => item.label}
-          shouldItemRender={(item, value2) => item.label.toLowerCase().indexOf(value2.toLowerCase()) >= 0}
-          inputProps={{
-            className: "form-control textBox",
-            placeholder: "Enter school name",
-            "aria-labelledby": "schoolNameText"
-          }}
-          wrapperStyle={{ display: "block" }}
-          value={this.props.value}
-          onChange={this.props.onChange}
-          onSelect={this.props.onSelect}
-          renderItem={this.renderDropdownItem}
-        />
-      </MDBContainer>
-    );
+  if (indexA !== indexB) {
+    return indexA - indexB;
+  } else {
+    return lowA < lowB ? -1 : 1;
   }
 }
 
+function shouldItemRender(item, value2) {
+  return item.label.toLowerCase().indexOf(value2.toLowerCase()) >= 0;
+}
+
+function SchoolPicker({ schoolInfo, onDoneLoading, value, onSelect, onChange }) {
+  const [ info, setInfo ] = React.useState(schoolInfo ?? { schools: [], schoolLinkDict: {} });
+
+  React.useEffect(() => void (async () => {
+    if (schoolInfo != null) {
+      onDoneLoading?.(this.state.schoolInfo);
+      return;
+    }
+
+    const response = await fetch(BACKEND + "/schools", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await response.json();
+
+    const schools = [ ];
+    const schoolLinkDict = { };
+    for (var i = 0; i < data.length; i++) {
+      schools.push({ key: i, label: data[i][0] });
+      schoolLinkDict[data[i][0]] = data[i][1];
+    }
+
+    setInfo({ schools, schoolLinkDict });
+    onDoneLoading?.(this.state.schoolInfo);
+  }), [ onDoneLoading, info, setInfo ]);
+
+  const renderDropdownItem = React.useCallback(item => (
+    <div key={item.key} className="auto_comp">{item.label}</div>
+  ), [ ]);
+
+  return (
+    <MDBContainer className="form-group">
+      <Autocomplete
+        items={info.schools}
+        sortItems={sortItems}
+        getItemValue={item => item.label}
+        shouldItemRender={shouldItemRender}
+        inputProps={{
+          className: "form-control textBox",
+          placeholder: "Enter school name",
+          "aria-labelledby": "schoolNameText"
+        }}
+        wrapperStyle={{ display: "block" }}
+        value={value}
+        onChange={onChange}
+        onSelect={onSelect}
+        renderItem={renderDropdownItem}
+      />
+    </MDBContainer>
+  );
+}
+
+export default React.memo(SchoolPicker);
