7a565112cb27a453ed3070ce2bb4e30407378e74
PrinxeShamar <44654743+PrinxeShamar@users.noreply.github.com>
2022-11-09 22:12:20 -0500

    update

--- PollBuddy-Server/frontend/src/pages/GroupJoin/GroupJoin.js
+++ PollBuddy-Server/frontend/src/pages/GroupJoin/GroupJoin.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "mdbreact/dist/css/mdb.css";
-import { MDBContainer } from "mdbreact";
+import {MDBContainer} from "mdbreact";
 import {withRouter} from "../../components/PropsWrapper/PropsWrapper";
 
 class GroupJoin extends Component {
@@ -15,72 +15,78 @@ class GroupJoin extends Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.updateTitle("Join Group");
   }
 
   handleEnterCode = () => {
-      fetch(process.env.REACT_APP_BACKEND_URL + "/groups/" + this.state.groupCode, {
-        method: "GET"
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response);
-          if (response.result === "success") {
-            this.setState({
-              showConfirm: true,
-              name: response.data.name,
-              description: response.data.description,
-            });
+    fetch(process.env.REACT_APP_BACKEND_URL + "/groups/" + this.state.groupCode, {
+      method: "GET"
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.result === "success") {
+          this.setState({
+            showConfirm: true,
+            name: response.data.name,
+            description: response.data.description,
+          });
         } else {
-            this.setState({groupCode: ""})
+          this.props.router.navigate("/groups");
         }
-        });
+      });
   };
 
   handleChange = (e) => {
     this.setState({groupCode: e.target.value});
-
   };
+
   async handleConfirmationResponse(join) {
     if (join) {
       let response = await fetch(process.env.REACT_APP_BACKEND_URL + "/groups/" + this.state.groupCode + "/join", {
         method: "POST",
       });
-      if(response.status === 200) {
+      if (response.status === 200) {
         this.props.router.navigate("/groups/" + this.state.groupCode);
         return;
       }
     }
     this.props.router.navigate("/groups");
-
-
   }
 
   render() {
     return (
       <MDBContainer className="page">
         <MDBContainer fluid className="box">
-          { this.state.showConfirm
+          {this.state.showConfirm
             ?
-              <MDBContainer className="form-group">
+            <MDBContainer className="form-group">
               <p className="fontSizeLarge">
-                Name: {this.state.name}
+                Group Name
+              </p>
+              <p className="fontSizeSmall">
+                {this.state.name}
               </p>
               <p className="fontSizeLarge">
-                Description: {this.state.description}
+                Group Description
+              </p>
+              <p className="fontSizeSmall">
+                {this.state.description}
               </p>
               <p>Are you sure you want to join this group?</p>
-              <input onClick={this.handleConfirmationResponse.bind(this, false)} className="button float-left" type="submit" value="No"/>
-              <input onClick={this.handleConfirmationResponse.bind(this, true)} className="button float-right" type="submit" value="Yes"/>
+              <input onClick={this.handleConfirmationResponse.bind(this, false)} className="button float-left"
+                type="submit" value="No"/>
+              <input onClick={this.handleConfirmationResponse.bind(this, true)} className="button float-right"
+                type="submit" value="Yes"/>
             </MDBContainer>
             :
             <MDBContainer className="form-group">
               <label>Please enter your group code:</label>
-              <input className="form-control textBox" type="text" name="groupCode" value={this.state.groupCode} onChange={this.handleChange}/>
+              <input className="form-control textBox" type="text" name="groupCode" value={this.state.groupCode}
+                onChange={this.handleChange}/>
               <input onClick={this.handleEnterCode} className="button float-right" type="submit" value="OK"/>
             </MDBContainer>
-        }
+          }
         </MDBContainer>
       </MDBContainer>
     );
