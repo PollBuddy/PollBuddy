587d953f02657e3ac0a913834559a59b051f5f37
MajorDroolz <maximhutz@gmail.com>
2023-02-17 23:18:06 -0500

    FIX: Error Text.

--- PollBuddy-Server/frontend/src/components/ErrorText/ErrorText.js
+++ PollBuddy-Server/frontend/src/components/ErrorText/ErrorText.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import "./ErrorText.scss";
 import { MDBContainer } from "mdbreact";
 
-export default class ErrorText extends Component{
-  render(){
-    return (
-      <MDBContainer className="errorContainer">
-        {/*pass in a text prop to customize the error text*/}
-        <p>{this.props.text === undefined ? "An error has occurred. Please try again later.": "ERROR: " + this.props.text + " Please try again."}</p>
-      </MDBContainer>
-    );
-  }
+function ErrorText({ text }) {
+  return (
+    <MDBContainer className="errorContainer">
+      {/* Pass in a text prop to customize the error text. */}
+      <p>
+        { text === undefined
+        ? "An error has occurred. Please try again later."
+        : "ERROR: " + text + " Please try again."}
+      </p>
+    </MDBContainer>
+  );
 }
+
+export default ErrorText;
